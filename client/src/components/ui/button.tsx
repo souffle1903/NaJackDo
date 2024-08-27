5c5502aec993c953e090e2fe2912d2ad6a588c82
이인준 <dldlswns890@naver.com>
2024-08-27 11:01:50 +0900

    shadcn 설치
    Former-commit-id: df96d40663b21dcc3b17b9681e9f61decb35ec6b

---
+++ client/src/components/ui/button.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b893be9323e3dde45c164fb766530825b80723f9848dfd99367bd3dd95c242e7
+size 1785
---
+++ client/tailwind.config.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0aa84af0c8a0b8384d5bed67867081d5bfcb6b39e058f61fce0507e9779dcfca
+size 2143
