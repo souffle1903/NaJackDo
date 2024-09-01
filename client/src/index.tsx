e470f8c635d1cc37392b24d1bccc36e806861eb3
lij0825 <dldlswns890@gmail.com>
2024-09-01 18:07:57 +0900

    [FEAT] sentry 적용
    Former-commit-id: 37ddad2e16964df1523f5aadc5b78e2fc7e06787

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66c9712ea153a8ff14dd1569c4281554d0ed4158e076023d778a0ff58e96eaab
-size 961
+oid sha256:0b5c5734709e0ece5ce46df6b87bd9f6169fb8ea8ee7a2391654ec05bee0eb6b
+size 2251
