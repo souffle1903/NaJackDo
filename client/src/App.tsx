613da076404b1e4e63319f689c72133aea48f877
lij0825 <dldlswns890@gmail.com>
2024-09-01 18:08:40 +0900

    [FIX] sentry test 주석
    Former-commit-id: 67768f4e6f05c4cd482537db5fc777fb33ac7446

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,8 @@
 import { Button } from "components/ui/button";
 import "./App.css";
 
-// sentry 테스트를 위해 오류 발생 테스트
-
 function App() {
+  // sentry test
   // function methodDoesNotExist(): void {
   //   throw new Error("Function not implemented.");
   // }
