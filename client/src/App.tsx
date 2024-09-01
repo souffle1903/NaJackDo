2dab21ff11c19567a3e64e609a81fd50f379f40a
lij0825 <dldlswns890@gmail.com>
2024-09-01 18:07:57 +0900

    [FEAT] sentry 적용
    Former-commit-id: cfe7e9b6524459961983919779c04d38b1f8320d

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,17 @@
 import { Button } from "components/ui/button";
 import "./App.css";
 
+// sentry 테스트를 위해 오류 발생 테스트
+
 function App() {
+  // function methodDoesNotExist(): void {
+  //   throw new Error("Function not implemented.");
+  // }
+
   return (
     <div className="App">
       <Button>Click me</Button>
+      {/* <button onClick={() => methodDoesNotExist()}>Break the world</button>; */}
     </div>
   );
 }
