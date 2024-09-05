172aa2a399ba4847d2c21fc7b15abd36c717b659
이인준 <dldlswns890@naver.com>
2024-09-05 12:15:09 +0900

    [FIX] 엔티티 테이블 이름 수정
    Former-commit-id: d9b219353ad9e53b47f2edc09983d1de0b0d6825

--- server/src/main/java/com/najackdo/server/domain/rental/entity/Rental.java
+++ server/src/main/java/com/najackdo/server/domain/rental/entity/Rental.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31d4ad3f3b1d10f0728bcd3a1fe98b1c9d3e5b0355a83b400a47e94c24f65687
-size 1553
+oid sha256:59e798f74cfcdae3b9c6d9fc55ab005703f9875a67581fc2348d5755d4b1325b
+size 1548
--- server/src/main/java/com/najackdo/server/domain/rental/entity/RentalLog.java
+++ server/src/main/java/com/najackdo/server/domain/rental/entity/RentalLog.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:93c3290ac7b50e193f85c14fa0283e93ef0ea53764ff816a26d5a16571862434
-size 1035
+oid sha256:e4fcf95592563bee8c9307a4b9c7c6ad365d0d7ed1ab57c53779e9aa6fbc4631
+size 1026
