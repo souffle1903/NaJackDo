efa7737c1239de94db59fb09b5193a7322a6dd94
lij0825 <dldlswns890@gmail.com>
2024-09-05 16:23:18 +0900

    [FEAT] 리뷰 엔티티 / enum 작성
    Former-commit-id: e76535e8793794c294358f3f780cfd26b1613dec

--- server/src/main/java/com/najackdo/server/domain/rental/entity/RentalSchedule.java
+++ server/src/main/java/com/najackdo/server/domain/rental/entity/RentalSchedule.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4cfea2e0a99d01d3442376b87699a1f00cecc751280c52c887b731386092fa2
-size 887
+oid sha256:010e87a7404901e95f9d0883a009d1a4cc3c087ea070b0a259f9ed8b490908c1
+size 1002
