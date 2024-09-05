b720350a503d4a28cde68f7bae39fc01cfd4b42b
이인준 <dldlswns890@naver.com>
2024-09-05 12:09:56 +0900

    [FIX] 엔티티 기본값 설정
    Former-commit-id: ca5b54fc54eacb76fc8fb961f9736764a5d2935e

--- server/src/main/java/com/najackdo/server/domain/book/entity/UserBook.java
+++ server/src/main/java/com/najackdo/server/domain/book/entity/UserBook.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0a71e98c6553844b2ec2479c3a048cbb713ea307d7665c9a303f80b088f4d11
-size 1978
+oid sha256:5886a190b6869b05ae96533870e9c2bc5bf77f9c60e6b3d084ad0e6feef116eb
+size 1947
--- server/src/main/java/com/najackdo/server/domain/user/entity/User.java
+++ server/src/main/java/com/najackdo/server/domain/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:287f64f8dd9d6f128eddb3a9ddfedabfed32fe7cb34b9a4fa29d7b7d4f4c129e
-size 3480
+oid sha256:5bb31e90dc577738d38fe762705dded3f188d7ea370650aa11d8642164966f0f
+size 3405
