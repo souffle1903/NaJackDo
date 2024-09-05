53d5879e39d540c7d392a66c5d6e07fe2a207b86
SeoungMin <hsm990423@gmail.com>
2024-09-05 21:02:21 +0900

    Oauth2 구현 중간 커밋
    Former-commit-id: 59b619aff5bf70bdb05bb95fb46cdf5ddc2e16e8

---
+++ server/src/main/java/com/najackdo/server/domain/auth/service/CustomUserService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3ea2d552f93e94604cadd65027a96f0282b5d44c7a053ec77205ca04af0bbfeb
+size 1369
---
+++ server/src/main/java/com/najackdo/server/domain/user/entity/User.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:acf01421e72bda334b60902abe3c76dc5fccf863ff4cc370e4be1922d18699be
+size 1955
