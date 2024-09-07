b6012d42f49dbe86e5d43b449b865ce9f3ea9617
lij0825 <dldlswns890@gmail.com>
2024-09-07 18:52:31 +0900

    [FIX] 쿼리 설정
    Former-commit-id: a36b4578341db15a2c9eb12c2176ea46d1ed13f8

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b5c5734709e0ece5ce46df6b87bd9f6169fb8ea8ee7a2391654ec05bee0eb6b
-size 2251
+oid sha256:97af55150be752a97f26b6f07d43bd1376438a73824aea96e53b6a2a7ac303bc
+size 1141
