c26761203ef41ca8b15abd08ebfc24771c5ee702
김도영 <rlaehdud1002@naver.com>
2024-09-07 20:29:07 +0900

    [FEAT] 검색 페이지 ui 하는중
    Former-commit-id: 046c3142c39e1f2904a26dda6abd9db6d845620d

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ad984bfe111b8f96140da1dca9a667023960803d90d58d0d87e06ba73c7c5b20
-size 915
+oid sha256:d66cf417d58ff268dc56da8ec3ae4d24112be5c6b39200865f5813196dabfb99
+size 1067
