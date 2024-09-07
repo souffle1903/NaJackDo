cee10c3eb8e57bf4f5da846132dfa81988820c3f
lij0825 <dldlswns890@gmail.com>
2024-09-07 21:12:34 +0900

    [FEAT] 카카오 페이 기능 페이지
    Former-commit-id: 2ea5ee61f0557820929b4f2195fcd24d88e692d9

--- client/src/page/kapay/page/KaPayFailPage.tsx
+++ client/src/page/kapay/page/KaPayFailPage.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-const KaPayFailPage: React.FC = () => {
+const KaPayFailPage = () => {
   return (
     <div>
       <h1>결제 실패</h1>
       <p>결제 처리 중 오류가 발생했습니다.</p>
-      {/* 오류 상세 정보를 표시하고, 다시 시도하거나 고객 지원에 문의할 수 있는 옵션을 제공할 수 있습니다 */}
     </div>
   );
 };
