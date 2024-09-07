0ea0d153065fd81dec7ae14e5693676e8b669215
lij0825 <dldlswns890@gmail.com>
2024-09-07 21:12:34 +0900

    [FEAT] 카카오 페이 기능 페이지
    Former-commit-id: 29f2716cae4e3e7027e6688a728a9e367b5d5748

--- client/src/page/kapay/page/KaPayApprovePage.tsx
+++ client/src/page/kapay/page/KaPayApprovePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6304d415791489fda74cfab3227fd46c05eb06d7dfd8f3c886580a3b67fef454
-size 682
+oid sha256:ddfb98dc31b8a37e23247535b3cf2fa07e7b52f72bc0529e7fe144d56ab1216d
+size 668
