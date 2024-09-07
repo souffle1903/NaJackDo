7cab00b05f4b3b1f62561d005d432ba65322da66
lij0825 <dldlswns890@gmail.com>
2024-09-07 21:12:34 +0900

    [FEAT] 카카오 페이 기능 페이지
    Former-commit-id: 89f2bc0cd36daaa6f89e4bfde39723b24842abac

--- client/src/page/kapay/page/KaPayCancelPage.tsx
+++ client/src/page/kapay/page/KaPayCancelPage.tsx
@@ -1,13 +1,3 @@
-import React from "react";
-
-const KaPayCancelPage: React.FC = () => {
-  return (
-    <div>
-      <h1>결제 취소</h1>
-      <p>결제가 취소되었습니다.</p>
-      {/* 사용자에게 다시 결제를 시도하거나 쇼핑을 계속할 수 있는 옵션을 제공할 수 있습니다 */}
-    </div>
-  );
-};
-
-export default KaPayCancelPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:1f8e623babbd57e681ca330a3ef4f4c975c14884a81121e6042ff85ed56bbec7
+size 662
---
+++ client/src/page/kapay/page/KaPayPage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f47f62f2b1454e7b543e22b18d876a7cc252fa13b2863c84708d780815b9b9d6
+size 2089
