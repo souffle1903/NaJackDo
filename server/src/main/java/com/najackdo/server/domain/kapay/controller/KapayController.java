3858be67f2edc463a8c16683a18ccfa423fd78cb
lij0825 <dldlswns890@gmail.com>
2024-09-07 17:44:36 +0900

    [FIX] 카카오 페이 기능 수정
    Former-commit-id: 5d7ea00becd20b9bee4bdd92dda542119eeb5131

--- server/src/main/java/com/najackdo/server/domain/kapay/controller/KapayController.java
+++ server/src/main/java/com/najackdo/server/domain/kapay/controller/KapayController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4234725d5fe4b1da67c0fc1dc15ce25be8ada4fb3c60ce5592b26ae2d4146458
-size 2608
+oid sha256:76eebb7e0ed802828f909b30654805124e2aaccc9220a6e0740125637636ec20
+size 2244
--- server/src/main/java/com/najackdo/server/domain/kapay/service/KapayService.java
+++ server/src/main/java/com/najackdo/server/domain/kapay/service/KapayService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7117904713f9afff25a0a70b450266fe552dad484ae95d93c7bdb0d4766da59
-size 3363
+oid sha256:2e52bfa5e0db74ad60065d53fc2cca256ad5bdfbcb29b0d75c0c459d1f9f620a
+size 3399
