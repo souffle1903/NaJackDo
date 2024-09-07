159433f7b8a6c6849ea708d28f8e75d03d0a359a
lij0825 <dldlswns890@gmail.com>
2024-09-07 17:44:36 +0900

    [FIX] 카카오 페이 기능 수정
    Former-commit-id: 74750e15bdf0b7a4a21aa90ab541f541991e8241

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbbac39d3bfbf126ca335ed28a203bcab5bd3a357690d71b3ac7b3d38136670d
-size 1579
+oid sha256:c3d7cbf674bee9e68d26c08fe4909c063bbe03cab19162dd403131de5a34a49d
+size 2957
