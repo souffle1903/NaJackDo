e4ed2e3870628b71ee781a7d9268c33f89501839
김도영 <rlaehdud1002@naver.com>
2024-09-08 19:42:55 +0900

    [FEAT] 검색 결과 페이지 ui 완성
    Former-commit-id: 7ca62d412a15dfc4ebe56ef999725da1e2ad0e66

---
+++ client/src/page/search/components/SearchResultBook.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:13c2f5e895f1f3463aa4fd7214f7da0aeafa4108581028ec6eec4f7cf31fd33c
+size 905
--- client/src/page/search/page/SearchPage.tsx
+++ client/src/page/search/page/SearchPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bc5f9c8a34ff26d3be9a760c20a76d8c85cba299e8f169c42df2e7f94741351c
-size 850
+oid sha256:2358ebab886f001e83171e18a6dcfd274d80d9e26177ea8faf422e210bd4c092
+size 1059
