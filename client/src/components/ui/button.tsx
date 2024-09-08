ca6b66de8cdab3c6b21dd64ae725d5f3ad21ce65
김도영 <rlaehdud1002@naver.com>
2024-09-08 23:37:25 +0900

    [FEAT] 로그인 페이지 ui 완성
    Former-commit-id: cbdeeeef0757df86bbc4275f57bbf529d0fa2d97

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a84a01d50c81e93a73d3ee55b50ed53f43b35613b6b36d05f577eb61ad1d872
-size 1847
+oid sha256:ab03387b64cd9c71bc4859b3332e2fe3af21a1e21b44a9c6fbe5ba84c9aa239c
+size 1834
