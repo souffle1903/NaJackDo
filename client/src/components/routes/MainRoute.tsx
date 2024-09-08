1abf5ca81585fa1bdfeb8adb9b141227d8f6517f
lij0825 <dldlswns890@gmail.com>
2024-09-08 16:28:52 +0900

    [FIX] 병합 후 안되는거 처리
    Former-commit-id: 0f0e06ee9346aa6a0ca6340927d1ac09d32b3d6d

--- client/src/components/routes/MainRoute.tsx
+++ client/src/components/routes/MainRoute.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f38312b5d218f74fafe738ab5b1f0c9832148875b67599f7793e73757e74d68b
-size 1030
+oid sha256:c22788011f6b4fb1df9c6cbe0451f9a6ca1506cdbe8067c999c747d192a22349
+size 1464
--- server/src/main/java/com/najackdo/server/domain/kapay/controller/KapayController.java
+++ server/src/main/java/com/najackdo/server/domain/kapay/controller/KapayController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35950bea6ba7ad0b29bbdfc360b8089ec2223ed3c066e72cff21008eeb63027d
-size 2849
+oid sha256:4837d40a031d0e7fc75a42e155be271788f35e11eeb5ff7dbe17ca7a226b867a
+size 2724
