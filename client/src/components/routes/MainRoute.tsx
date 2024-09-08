2d8daac0dd04d1573f36cc16898579bb3ff904b2
김도영 <rlaehdud1002@naver.com>
2024-09-08 19:42:55 +0900

    [FEAT] 검색 결과 페이지 ui 완성
    Former-commit-id: b99e8794846d14db311084841d5087e47cd8441b

--- client/src/components/routes/MainRoute.tsx
+++ client/src/components/routes/MainRoute.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f38312b5d218f74fafe738ab5b1f0c9832148875b67599f7793e73757e74d68b
-size 1030
+oid sha256:41dccd15ba2c95d0e9aa26a4592b1a46dac267ace80a1b528e3e2fb22d0f9b70
+size 1165
