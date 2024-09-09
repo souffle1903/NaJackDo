d4fd93db7c1c3b987ed8b164186cf7011009ca6f
chung <souffle1903@gmail.com>
2024-09-09 19:52:40 +0900

    [FEAT] 지역 설정  페이지 UI
    Former-commit-id: 185f94482f29432ce3f444f507527b54e92940e3

--- client/src/components/routes/MainRoute.tsx
+++ client/src/components/routes/MainRoute.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fea329b05f267ea7d4b84a48a77433ac714a233a983bfe419fb5e11d84159257
-size 2308
+oid sha256:306c6cc845469af98c6b9f4a02d1dc94b601b0ec65c01d75001ae5ded901cd52
+size 2457
--- client/src/page/location/page/LocationPage.tsx
+++ client/src/page/location/page/LocationPage.tsx
@@ -1,9 +1,15 @@
-import KakaoMap from "./component/KakaoMap";
+import { useNavigate } from "react-router-dom";
+
 const LocationPage = () => {
+  const navigate = useNavigate();
+  const goToLocationSetting = () => {
+    navigate('/location/setting');
+  };
   return (
     <div>
       <h1>Location</h1>
-      <KakaoMap />
+      <button onClick={goToLocationSetting}>임시임시 지역 설정 </button>
+
     </div>
   );
 };
