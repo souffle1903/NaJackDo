21277a3feb4156e18360fe357c36271699aed35c
ParkBeomSu <beomsu4639@gmail.com>
2024-09-09 10:50:06 +0900

    save
    Former-commit-id: 55525691cba821973355cf593135b7f88ba24c04

--- server/src/main/java/com/najackdo/server/core/util/AladdinOpenAPI.java
+++ server/src/main/java/com/najackdo/server/core/util/AladdinOpenAPI.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0ede19887c59df8e5c4108351985fa38ab00b6e10557333e14fcf69fe4c21fd
-size 7959
+oid sha256:27f386cbe4ee2182c061739ea0985725904cbde9c206c080e32e5e803cd21a34
+size 7956
