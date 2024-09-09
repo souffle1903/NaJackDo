7558830f7bcf4c44ea4969bb90ff6ca95bf4d639
이인준 <dldlswns890@naver.com>
2024-09-09 18:00:00 +0900

    [FEAT] firebase 의존성 추가, 초기 설정 추가
    Former-commit-id: 41fde263f7c88ada04026233caa4b63bc6f778db

---
+++ server/src/main/java/com/najackdo/server/core/configuration/FCMInitialize.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1d67d3c324c5b5900763d9a17dd12edd5dbbf6bc562faac197277c0c6f5160ce
+size 1094
--- server/src/main/java/com/najackdo/server/domain/notification/entity/NotificationType.java
+++ server/src/main/java/com/najackdo/server/domain/notification/entity/NotificationType.java
@@ -1,5 +1,14 @@
 package com.najackdo.server.domain.notification.entity;
 
 public enum NotificationType {
-	BOOK_RENTAL_REQUEST, BOOK_RETURN_REMINDER, CHAT
+	
+	BOOK_RENTAL_REQUEST, // 책 대여 요청
+	BOOK_RENTAL_APPROVAL, // 책 가능 승인
+	LIKE_BOOKCASE, // 책 좋아요
+
+	// 책 반납 7일전, 3일전, 1일전
+	BOOK_RETURN_REMINDER_7DAYS,
+	BOOK_RETURN_REMINDER_3DAYS,
+	BOOK_RETURN_REMINDER_1DAY,
+
 }
