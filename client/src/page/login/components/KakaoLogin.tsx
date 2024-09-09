c3160bd3d30fbee0dc6073db1bc4514cc6132d97
김도영 <rlaehdud1002@naver.com>
2024-09-09 10:46:59 +0900

    [FEAT] header 조건 추가
    Former-commit-id: 6ed49231fe5da77181f1c921d8d6b228dc7b85ae

--- client/src/page/login/components/KakaoLogin.tsx
+++ client/src/page/login/components/KakaoLogin.tsx
@@ -1,5 +1,5 @@
 import { RiKakaoTalkFill } from "react-icons/ri";
-const Kakao = () => {
+const KakaoLogin = () => {
   return (
     <div className="flex flex-row items-center bg-[#FEE500] px-4 py-2 rounded-xl">
       <RiKakaoTalkFill size={32} className="mr-2" color="#3B1E1E" />
@@ -8,4 +8,4 @@ const Kakao = () => {
   );
 };
 
-export default Kakao;
+export default KakaoLogin;
