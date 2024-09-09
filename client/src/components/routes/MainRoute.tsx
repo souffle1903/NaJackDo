d04637519ad1c00ed93c0cb50207d4e889d743b2
Chung <souffle1903@gmail.com>
2024-09-09 17:56:53 +0900

    [FEAT] 좋아요리스트 페이지 UI
    Former-commit-id: 7f5636db011840ad7fe5bd792ca0d3ec5266acf0

--- client/src/components/routes/MainRoute.tsx
+++ client/src/components/routes/MainRoute.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e1f8a620a5182217b1dc4bef1a084697d5d772765367300559ae06b6890e40ec
-size 1930
+oid sha256:358e2d7ae54839a1ae54b98687ec41b35fc26144fcefb1d5b9ce510ad44460fc
+size 2072
--- client/src/page/location/page/LocationPage.tsx
+++ client/src/page/location/page/LocationPage.tsx
@@ -1,7 +1,9 @@
+import KakaoMap from "./component/KakaoMap";
 const LocationPage = () => {
   return (
     <div>
       <h1>Location</h1>
+      <KakaoMap />
     </div>
   );
 };
