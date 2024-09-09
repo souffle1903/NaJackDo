0b8a1386e593c787f76e28aae4c7920b9f9b839d
chung <souffle1903@gmail.com>
2024-09-09 19:52:40 +0900

    [FEAT] 지역 설정  페이지 UI
    Former-commit-id: 74faae531c81d8a3b7bc7a0e084d3239afa551cd

--- client/src/page/profile/page/GradePage.tsx
+++ client/src/page/profile/page/GradePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16e0fc58365f0bda79430750d76ac17694947f3e625aab894a4b8a7c74b6eef7
-size 2073
+oid sha256:275e5c484d5a8089eb81499943d5c4aadea1b53da8d9c8c0f52a72e19d38b9e6
+size 2074
