7ef1d3e374f3712a76f3f2b8c25cc89c300ca6eb
lij0825 <dldlswns890@gmail.com>
2024-09-09 21:54:03 +0900

    [FEAT] firebase 설정
    Former-commit-id: f2d24addb8317feaea269d6330960dc1cf82f47c

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a39c42083b600c97bc5c84dd76a69479036e9da7fbc4961fe263906481e9a025
-size 990
+oid sha256:b911c319275bdefe5a519ef54c58a7ad190d2ee15a882e0ad7f4a926ef818ec0
+size 1834
