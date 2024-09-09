e649b28844e99efee68db4df021bc99ea52f70c4
chung <souffle1903@gmail.com>
2024-09-09 20:53:36 +0900

    [FEAT] 지역 조회 페이지 UI close #9
    Former-commit-id: 72359f04f880c264b1cbec525e6f788588897e59

--- client/src/page/location/page/LocationSettingPage.tsx
+++ client/src/page/location/page/LocationSettingPage.tsx
@@ -1,12 +1,3 @@
-import KakaoMap from "../components/KakaoMap";
-
-const LocationSettingPage = () => {
-  return (
-    <div>
-      <h1>Location</h1>
-      <KakaoMap />
-    </div>
-  );
-};
-
-export default LocationSettingPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:8ac3094aae4b1e1d7cc3a405e7c122e12d70a59d4e84a5cb1fd407120e771559
+size 592
