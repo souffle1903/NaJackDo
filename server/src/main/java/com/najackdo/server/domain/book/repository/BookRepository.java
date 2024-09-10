eb2e0666dbce90ba445f0ae527faa917ddd4bc17
ParkBeomSu <beomsu4639@gmail.com>
2024-09-10 14:54:18 +0900

    [FEAT] 수정
    Former-commit-id: 5589705df8372cf5586d9ae4e4ada7a3d816a688

--- server/src/main/java/com/najackdo/server/domain/book/repository/BookRepository.java
+++ server/src/main/java/com/najackdo/server/domain/book/repository/BookRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long>{
 
-    @Query("SELECT b FROM Book b WHERE b.title LIKE concat('%', :keyword, '%')")
+    @Query("SELECT b FROM Book b WHERE b.title LIKE concat('%', :title, '%')")
     Book findFirstByTitle(@Param("title") String title);
 }
