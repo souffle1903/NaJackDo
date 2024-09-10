8c7eeb366d826e27faffd07172afc52a9d76a755
Chung <souffle1903@gmail.com>
2024-09-10 15:15:09 +0900

    [FIX] 지역 설정 페이지 UI 행정동 반환 close #8
    Former-commit-id: 1449deddecc8bbf82d8af408e000a6ee6b6091a9

--- client/src/page/location/components/KakaoMap.tsx
+++ client/src/page/location/components/KakaoMap.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:810b459c638b780ddf371af4e988a4c5b9285c6cbf9acfeb304c4f818aa288da
-size 2848
+oid sha256:ada5b048488a5180573e595d76083dd5205f3d0081fa7309b3a273767522a35f
+size 3193
