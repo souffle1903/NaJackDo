cac551e80ce24b155ada3f709a9935f55324a587
서민수 <minsu9812@naver.com>
2024-09-10 13:58:52 +0900

    [FEAT] 설문조사 페이지 UI
    Former-commit-id: 5688ad36fdfadb73f5f267b0748c8ac8d53b54fc

--- client/src/page/login/components/Consent.tsx
+++ client/src/page/login/components/Consent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df923c8ef339b82a34f76228ecf81dff8f9a46a99e11feb945c458a9556404ed
-size 1099
+oid sha256:4f74f1fd9bda6f45d29df8064063832c601a2ae053d94324cdcc1e7605e86583
+size 4038
