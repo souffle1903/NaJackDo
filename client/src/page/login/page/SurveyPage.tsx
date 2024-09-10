219af8197c9e1d39a48f380cd36cd87300989be1
서민수 <minsu9812@naver.com>
2024-09-10 15:59:43 +0900

    [FEAT] 설문조사 단계 UI 완료
    Former-commit-id: fe21d480a8c62f71cbbbb4ad2c22854dfc8bf9b9

--- client/src/page/login/page/SurveyPage.tsx
+++ client/src/page/login/page/SurveyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b8faa262a0f8d0a84bd344266aad64370ecc42baae1e9d509637548d51b0063
-size 879
+oid sha256:cc3106303b656688ea989a5d978974c864d6ee4f0180fa7245b417499702cf40
+size 2505
