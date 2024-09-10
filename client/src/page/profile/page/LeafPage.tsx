2c8247f99c215a3aab3ee6f8fc4a92359b8769c1
Chung <souffle1903@gmail.com>
2024-09-10 17:39:15 +0900

    [FEAT] 책잎 충전 페이지 UI close #15
    Former-commit-id: d6a096a70bb018155a76b431bb3b72145b05f890

--- client/src/page/profile/page/LeafPage.tsx
+++ client/src/page/profile/page/LeafPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:108859eb0ada0ee25a31810f44b1e5abd528e532fc499c4222daf048ac96359f
-size 1515
+oid sha256:899eba88e4be2f71261e0ebff143c96bd44622db7b99ac6bdc92a814c951a552
+size 1807
