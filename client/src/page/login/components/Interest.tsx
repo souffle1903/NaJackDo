ea4d5b6d0ed4bde06ccfb1cd27ca98b708633fb5
서민수 <minsu9812@naver.com>
2024-09-10 17:49:55 +0900

    [FEAT] 서재 UI 퍼블리싱
    Former-commit-id: 45279861de4d29a7eea8595455ad6b7a00bbf85d

--- client/src/page/login/components/Interest.tsx
+++ client/src/page/login/components/Interest.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:999c2d8ee046c671f8633dd02adbc3063d3ff592a0540aa4ae734f5c1fad6c46
-size 2635
+oid sha256:bb34bb20a0549dd3456663b3d8515381bda24fff396210277db9e5ed93a46d71
+size 2517
