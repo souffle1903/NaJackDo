109d6e80aba28b99530df7e4e94d90710b8cdae3
김도영 <rlaehdud1002@naver.com>
2024-09-11 16:48:18 +0900

    [FEAT] 도서 등록 - 촬영 후 ui 완성
    Former-commit-id: 0e3351491ea62874f1beb492ade57f1d91998b0b

--- client/src/page/main/page/MainPage.tsx
+++ client/src/page/main/page/MainPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8efc10846f605eb079539531b5d3804af0434e7de59882d68f4847ba89b43255
-size 652
+oid sha256:16e39d44cd14a60ba35407d27bca14c247bbf8491ca38637643d4f2317eeb729
+size 647
