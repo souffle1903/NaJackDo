24f249df4f9b3ab65a20302e2189557ce8be31de
이인준 <dldlswns890@naver.com>
2024-09-11 17:26:12 +0900

    [FEAT] User API / 유저 닉네임으로 조회
    Former-commit-id: 9b06de43b0d08e8c5370b7431553095afa70a42e

--- server/src/main/java/com/najackdo/server/domain/user/repository/UserRepository.java
+++ server/src/main/java/com/najackdo/server/domain/user/repository/UserRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a4fbb3f98b6ce49bf8a561cd81418323bcbc22c7e8d537d9eb7cc5494434066
-size 704
+oid sha256:de516cf1ef8a72a640b8e4f63a4b1419ea1c52b8f312dea2ffdc28f4dedaf988
+size 903
