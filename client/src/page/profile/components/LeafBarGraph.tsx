6536a0bb30d5d3132233a7af21a98034c491b0a9
Chung <souffle1903@gmail.com>
2024-09-11 16:42:51 +0900

    [FIX] 프로필 페이지 UI 추가 close #20
    Former-commit-id: ca7da6e6aab1c4d23bccecbbf6f1f146a188af1b

--- client/src/page/profile/components/LeafBarGraph.tsx
+++ client/src/page/profile/components/LeafBarGraph.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60b940527397db8a24c34380ee1a912c134ec78c6fd6f720a842e6b6837b6790
-size 824
+oid sha256:6bdce6fa4da9c74143eae27738165ec70d940cd9f62cd31251b4cf6452a7e464
+size 840
