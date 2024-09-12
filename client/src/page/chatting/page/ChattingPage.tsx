f2aa6b59c25511a1277655e3c0af866d48203d60
김도영 <rlaehdud1002@naver.com>
2024-09-12 17:18:02 +0900

    [FEAT] 채팅 리스트
    Former-commit-id: d831e1953aa90ed747962ca03313c8e84d74efa6

--- client/src/page/chatting/page/ChattingPage.tsx
+++ client/src/page/chatting/page/ChattingPage.tsx
@@ -1,9 +1,3 @@
-const ChattingPage = () => {
-  return (
-    <div>
-      <h1>Chatting</h1>
-    </div>
-  );
-};
-
-export default ChattingPage;
+version https://git-lfs.github.com/spec/v1
+oid sha256:5213bbd26abe8ef9cf0e0f6affd57545273205339cee7d110a74ff217a2dac09
+size 667
