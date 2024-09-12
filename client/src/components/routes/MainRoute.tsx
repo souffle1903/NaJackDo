3f8cb477e18475fe8a1c017b7fc371fbc1eb227c
서민수 <minsu9812@naver.com>
2024-09-12 17:36:56 +0900

    [FEAT] 후기보내기 페이지 UI  진행중
    Former-commit-id: 81c89e4d0a3bb309387bd523e7902afec1cace90

--- client/src/components/routes/MainRoute.tsx
+++ client/src/components/routes/MainRoute.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5f91101d153d6d2d7f46ede246a7ecf99a0e82deb52f401e08dab7fd9c5ff01
-size 3916
+oid sha256:3b19eadec61fed8695eddbdc6e4c53c8b440fe2b582561ef28c0e389ff29b9b1
+size 4033
