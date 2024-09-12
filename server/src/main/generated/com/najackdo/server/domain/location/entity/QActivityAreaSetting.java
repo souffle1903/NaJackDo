0519be292b30e5d3c91073dd25dba3a2dd556da9
ParkBeomSu <beomsu4639@gmail.com>
2024-09-12 16:27:39 +0900

    [FIX] generated 제거
    Former-commit-id: 4515dee5dd0c2b54a179502d07db8626aeb2151a

--- server/src/main/generated/com/najackdo/server/domain/location/entity/QActivityAreaSetting.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:64195dfa8b451ae94d170bc4be69edb20e59e603e2ed1a54df767aa1746541fa
-size 2242
--- server/src/main/generated/com/najackdo/server/domain/rental/entity/QRentalSchedule.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:2e1a51b387d9c069a8f1b8b50fe5b4dc5a30722e6bf82130c4339f62757690d3
-size 1421
