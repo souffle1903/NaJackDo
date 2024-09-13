cfcffccc0d1740b015c42c126124e47bcbbe886a
김도영 <rlaehdud1002@naver.com>
2024-09-13 14:22:46 +0900

    [FEAT] baseresponse 추가
    Former-commit-id: c86f08383b028725ee06f30558c077db89c7ac99

--- client/src/components/routes/MainRoute.tsx
+++ client/src/components/routes/MainRoute.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5f91101d153d6d2d7f46ede246a7ecf99a0e82deb52f401e08dab7fd9c5ff01
-size 3916
+oid sha256:6cc5cd1d04fed33e285e9b288b9288f8fa60549f64c5cf84d894ca86bdd661a8
+size 4052
---
+++ client/src/page/chatting/page/ChattingRoomPage.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4464893cc4bc6a45a45d760af498607845f6cefcf517879c66cacf98d61a6da9
+size 1205
