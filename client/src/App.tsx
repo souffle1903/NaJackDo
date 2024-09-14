7e878133a2cb02e08ad58c7ee8d141fe1ceb86ec
lij0825 <dldlswns890@gmail.com>
2024-09-14 20:24:56 +0900

    jenkins test
    Former-commit-id: 465ccf49c0403f7de29f08839669dacc5c49d6aa

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18fe1e497adfb1270e3cc98b3972e154713f0ffe0f6bd79b986b488015bae945
-size 1012
+oid sha256:5d3b4bf800f05eb7b32480e51578e6f7304360198b7f91559198fad8018e2982
+size 1002
