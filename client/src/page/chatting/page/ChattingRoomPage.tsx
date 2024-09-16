8c9acfffd3ec7a051f2935cb92aa5b0e407cd9d1
김도영 <rlaehdud1002@naver.com>
2024-09-16 13:38:21 +0900

    [FEAT] 컴포넌트 분리
    Former-commit-id: 87d8d3a897ad7ee4f82ac55f971d26b86049dab3

--- client/src/page/chatting/page/ChattingRoomPage.tsx
+++ client/src/page/chatting/page/ChattingRoomPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41d5d9f7abe0b4775f1b5ea19c2d28c2ecad3d1040d856e45bdac014a6ad8b04
-size 5682
+oid sha256:cca8f5e1abc6326c7a9fb1ecc7ce8b1a474e639325cbe22662ba8d201c2a8c4c
+size 5014
