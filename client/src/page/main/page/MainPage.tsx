cfe09faeeb30311e33ebcaf9308cc6cc436fe3e2
김도영 <rlaehdud1002@naver.com>
2024-09-16 23:25:01 +0900

    [FEAT] jenkins notification test
    Former-commit-id: ab939c1bec1deea51a55c918d1e283e523cbb3ef

--- client/src/page/main/page/MainPage.tsx
+++ client/src/page/main/page/MainPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:329eedd9bcb337ecce6b3c96f3d50bb7f8721d53c8ddf382037b5a6bcc17a32b
-size 1476
+oid sha256:dd6dbd277705c510a2e26e506f39b66e389f6b1641dc7d515cf3f5a69c7a83e1
+size 1472
