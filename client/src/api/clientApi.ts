3545619659dc832e4d534d9a0134df161d6a60c4
lij0825 <dldlswns890@gmail.com>
2024-09-18 18:00:06 +0900

    [FIX] clientapi 수정
    Former-commit-id: 58f4f3f0ebb44e09d816e0e85da3a78af980e962

--- client/src/api/clientApi.ts
+++ client/src/api/clientApi.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c29a9b4eff4bd1b399b502d466abb4d0763c428a5a2079d860dd33e4ba40c0c
-size 2542
+oid sha256:21acc472480c2ea9bbe8a2d4e05689915c9876aeaa10e3804ead62ba2483f985
+size 2530
