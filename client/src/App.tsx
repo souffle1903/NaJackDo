a467ba5e485aa81d1e893c7259aa2368cd5da635
lij0825 <dldlswns890@gmail.com>
2024-09-18 21:17:52 +0900

    [FIX] login -> signin
    Former-commit-id: f310285e5cb4ee0b4f4f1de35052d15956e36bc7

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5d3b4bf800f05eb7b32480e51578e6f7304360198b7f91559198fad8018e2982
-size 1002
+oid sha256:b077e6ec1013d5afc7bb66a2f005cea52dd544918995abe8a182d961337704ee
+size 1003
