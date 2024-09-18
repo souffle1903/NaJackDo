e55813205e6448e0bfb752dc0977f5abe72859a1
ParkBeomSu <beomsu4639@gmail.com>
2024-09-18 15:03:25 +0900

    [FIX] final 추가
    Former-commit-id: ea711cb51033f031c60494180af2765cc92e85e7

--- server/src/main/java/com/najackdo/server/domain/book/service/UserBooksService.java
+++ server/src/main/java/com/najackdo/server/domain/book/service/UserBooksService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f00ff3ec47d1f3c7f54b98f646da459f4926e0bd3582d374286519cde296077
-size 1782
+oid sha256:f5213e2c4701ac138b5ebfeff86532558ad32144339c8181ebdb42b51eefe172
+size 1800
