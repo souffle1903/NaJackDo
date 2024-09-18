b939cd0bcff846ed603690901616687f6de32bc1
SeongMin Hwang <qq221qq@naver.com>
2024-09-18 16:19:05 +0900

    [FEAT] 최근 검색어, 인기 검색어 구현
    Former-commit-id: 3e8b0bcafafe53c844f872c40290c2d43a1ea5ef

--- server/src/main/java/com/najackdo/server/core/configuration/RedisConfig.java
+++ server/src/main/java/com/najackdo/server/core/configuration/RedisConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:80d4e25e2ca4470565831938982915970901bb03ebfac18fafae13f0bea77d26
-size 2327
+oid sha256:235f981b9383350f6d866bad9a983074618db51640ca5bae84a4260a8e4b6b52
+size 2874
