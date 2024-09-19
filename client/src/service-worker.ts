680d21fbc4ac896aec476ddc6dada35c4baa9811
이인준 <dldlswns890@naver.com>
2024-09-19 13:49:03 +0900

    [FIX] 서비스 워커 white list 추가
    Former-commit-id: 9811b8b491fb375992cb862b158e1710f7765c0d

--- client/src/service-worker.ts
+++ client/src/service-worker.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc79bacf36443149c21faca689325c75e43eb3736367a91fb8ba84b74e682ea5
-size 2974
+oid sha256:28a25c3e9ad3ce5be2e6559c0c8ae70138f86d740260660d350e87759a3a5ecb
+size 3450
