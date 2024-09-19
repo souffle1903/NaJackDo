2384f6967fe4ac705527271eb97101afffd5b4ea
김도영 <rlaehdud1002@naver.com>
2024-09-19 14:17:31 +0900

    [MERGE]
    Former-commit-id: 235893a16a05fca8b1393bec6c71d6ebc244311a

--- client/src/page/login/components/KakaoLogin.tsx
+++ client/src/page/login/components/KakaoLogin.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:766d477b98075e874bb228e024f5157d47ed83886a30ccee58a546226a53956f
-size 636
+oid sha256:be8d0644aed05b2726f805f62a2a17fecf57dd7c5c5617a088922ecac59102fc
+size 691
