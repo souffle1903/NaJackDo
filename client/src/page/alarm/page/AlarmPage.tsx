9551234ad7bc83fba4d740d7bcf60ac5332f0484
서민수 <minsu9812@naver.com>
2024-09-19 17:03:24 +0900

    [FEAT] UI 폰트,위치 통일
    Former-commit-id: 122b4a99a7ea96c514a8e96d1a79cb3b3d924fd9

--- client/src/page/alarm/page/AlarmPage.tsx
+++ client/src/page/alarm/page/AlarmPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:930fa82a7f1623509b33937d034d694e374021078d67185bb7ac6f6b92dbd1d6
-size 1827
+oid sha256:76508dce886beaf7b445f497e50fa1df7679ec3f5b5f6d3f0e988fdeec837741
+size 2076
--- client/src/page/main/components/C105Recommend.tsx
+++ client/src/page/main/components/C105Recommend.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c53f193b4c7f23b74eeb0c054a68e847c953a3d4cb50397326478ae2318636fb
-size 728
+oid sha256:8a6f236914921120c4937dfed733a96b288b77820f47ad063ddcd3342ab78431
+size 868
