3649834ebae67a0cc793eb0148b73804a73e65cb
SeongMin Hwang <qq221qq@naver.com>
2024-09-20 11:34:38 +0900

    [FIX] 긴급패치
    Former-commit-id: 828699fa7ff7e96ef8d3b83ccbdf2627c5c97ec2

--- server/src/test/java/com/najackdo/server/domain/search/controller/SearchControllerTest.java
+++ server/src/test/java/com/najackdo/server/domain/search/controller/SearchControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6245dd17f92e65c221e60302cf1892b50a8ca76fbe3cceeb1577d0525e5875bd
-size 9819
+oid sha256:324eb111185e0e3eaf0e5845a2d5677c4fadbb48959681ebecd632022ded4595
+size 10507
