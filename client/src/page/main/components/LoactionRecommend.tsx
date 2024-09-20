f0802f00823047d8bf936b15ee70ae2946b3f509
서민수 <minsu9812@naver.com>
2024-09-20 10:00:01 +0900

    [FEAT] ui 위치 조정
    Former-commit-id: 7d8eadf28432b265d598cfa1bc70cf15632ddce9

--- client/src/page/main/components/LoactionRecommend.tsx
+++ client/src/page/main/components/LoactionRecommend.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77e248f51fd35bc768b2c7027ae7d4df3f3f04428e23ca00c530f4facb831e79
-size 2176
+oid sha256:89f2676320a3bd0ddbe5d992f90b4b414f934a1edc5a1891286bad2088876188
+size 2159
