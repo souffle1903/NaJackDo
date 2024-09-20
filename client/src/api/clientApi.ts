1b5c31c4b780402f490637c951e094622b076055
김도영 <rlaehdud1002@naver.com>
2024-09-20 10:25:49 +0900

    [FEAT] 자동완성 검색어
    Former-commit-id: a28bd79e9f5aeed790b672d49715b6004784aed2

--- client/src/api/clientApi.ts
+++ client/src/api/clientApi.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e872bc8a68709a2a5e7e0b1da901837913149027fc988d4325bfe5a8d938ec9
-size 2369
+oid sha256:e66fdcd0fa9c013a6ebaa00099705da50ca6775fe03e57e46614f7c1c1c4849f
+size 2377
--- client/src/page/search/components/PopularSearch.tsx
+++ client/src/page/search/components/PopularSearch.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae606d89cfacea65e1976312002e4d3341046b90081f4d4a318ba47797c46712
+oid sha256:3184b2764ef32c39be18ba63663385755f18c72ad196dcbe579acd612a6f883d
 size 691
