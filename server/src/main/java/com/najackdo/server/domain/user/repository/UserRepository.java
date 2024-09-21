0e1a5d4b3362f9c87c78245eaf1933c48b3c61c3
lij0825 <dldlswns890@gmail.com>
2024-09-21 23:52:45 +0900

    [FEAT] 닉네임만 조회 api / 닉네임 중복 확인 api / 업데이트 api / front - back 연결
    Former-commit-id: efba6d6f88a9cb1c65385f734cc8751db29f826f

--- server/src/main/java/com/najackdo/server/domain/user/repository/UserRepository.java
+++ server/src/main/java/com/najackdo/server/domain/user/repository/UserRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de516cf1ef8a72a640b8e4f63a4b1419ea1c52b8f312dea2ffdc28f4dedaf988
-size 903
+oid sha256:a6a8a49f0b82c488c60dc70aa793fa28a114e55630217720b5f8416a46bdf1cb
+size 905
