a68887c3bcccec06f73bb3fbfb80439b86c9fd1c
lij0825 <dldlswns890@gmail.com>
2024-09-22 00:04:30 +0900

    [FIX] test 주석
    Former-commit-id: 2ec24b5f4615c4141a24d660433542f6e7a16c7b

--- server/src/test/java/com/najackdo/server/domain/user/controller/UserControllerTest.java
+++ server/src/test/java/com/najackdo/server/domain/user/controller/UserControllerTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac0923229ccc07c46661007e10e2ace16ed2421194ddbb9c38e25688fd4fd0ef
-size 3581
+oid sha256:806a529c4c089b130952eebd864d3eeee5e49abca690e5034f19c5fb361828b6
+size 3837
