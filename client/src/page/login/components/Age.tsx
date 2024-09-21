9da69f45f74e68341f8b59444aa32ef8472ca1bd
lij0825 <dldlswns890@gmail.com>
2024-09-21 23:58:04 +0900

    [FIX] survey 수정
    Former-commit-id: 3793a3380ae78a6d65676a528745ab97f4295cf8

--- client/src/page/login/components/Age.tsx
+++ client/src/page/login/components/Age.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9837f5dab1182e99d0e38890c2ceb3e8fbb4b9d1163a35fb5a06bc885db484b
-size 3575
+oid sha256:648a9cb8c625f7b27d7b93cae6fbb6377b974b02f506bd110bbebfb23be7675c
+size 3196
--- client/src/page/login/components/Nickname.tsx
+++ client/src/page/login/components/Nickname.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d506150ccb5a3b440fd33cc96ff17e0fb5921ccb6b0852e17523d2aed298b73d
-size 939
+oid sha256:c372add36cdc5fee7d93a9b02e0009d34fde8b28d65dac4dad739462716eaf18
+size 2938
