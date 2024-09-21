8969766c8e3ff4f192da75554abee50ce8b46f26
lij0825 <dldlswns890@gmail.com>
2024-09-21 17:21:53 +0900

    [MERGE] 병합 충돌 해결
    Former-commit-id: 93827f34d3ac5f37c14267e8f958478e21f0f4ff

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e9df1ba2719dcee440ecd09609a5bfea9f04909a68c68b012607294de2b08884
-size 3134
+oid sha256:39c79b0760cd04bfc0fc2d449f77868e0837c53644715d4ebc7fa21aa998a5c2
+size 3175
--- server/src/main/java/com/najackdo/server/domain/book/entity/BookMark.java
+++ server/src/main/java/com/najackdo/server/domain/book/entity/BookMark.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8d736b5c2bea3e60a2650cb3de1766c2d6aa7dab16e7144130f8d3dd5949d3a
-size 1153
+oid sha256:68bc18d1c3bd7cff097d23b9c59498519ef3e86f5bb43757c366a1dde18551cf
+size 1204
