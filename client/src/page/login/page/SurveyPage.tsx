7522f119c6cb1926bd704b517d0d852ab832c49d
lij0825 <dldlswns890@gmail.com>
2024-09-21 23:58:04 +0900

    [FIX] survey 수정
    Former-commit-id: d492b591fc13dc9a17bc24025058592d7e6d7586

--- client/src/page/login/page/SurveyPage.tsx
+++ client/src/page/login/page/SurveyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc3106303b656688ea989a5d978974c864d6ee4f0180fa7245b417499702cf40
-size 2505
+oid sha256:ab9a1f6124875f3679916cf10bbd90cadd12bf102b8ea193731cd2dc385449f0
+size 3262
