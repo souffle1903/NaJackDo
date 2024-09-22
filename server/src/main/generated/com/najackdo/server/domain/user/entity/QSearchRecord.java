5355259a52a98e1b18d85bae47ec0759cf260c10
SeongMin Hwang <qq221qq@naver.com>
2024-09-22 12:08:56 +0900

    [DOCS] 스웨거 적용
    Former-commit-id: a8f33634b0f3921ca29e84a1810d33f942960adf

--- server/src/main/generated/com/najackdo/server/domain/user/entity/QSearchRecord.java
+++ server/src/main/generated/com/najackdo/server/domain/user/entity/QSearchRecord.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac967f126b2d8909c9b70c96b34828df1caa45e72e12fd522c71b0080cbf6f1a
-size 1887
+oid sha256:b3c877e27fd2a8361fefe1262b9768ed2f4c737cfcc59ea888640096a2a3841d
+size 1906
