5933e875540554c771447f99cc1c7f1fd9cf0bd7
DESKTOP-6ABJ64L\SSAFY <poj4639@ajou.ac.kr>
2024-09-23 18:02:16 +0900

    [FEAT] 알람 구현 및 테스트, 리팩토링 종료, 알람 두번 오는 버그잇음
    Former-commit-id: 1138b90e62a1dbff77519e09920dbd362d6d9af8

--- client/public/firebase-messaging-sw.js
+++ client/public/firebase-messaging-sw.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86855ee920bf5bbd90750039a3826905bdde78b3bf2cd5d9d13abe9ab805467d
-size 1252
+oid sha256:a5386f384c51d074312c869ceb31e2451305b0932a7a984de755dd3324078139
+size 1297
