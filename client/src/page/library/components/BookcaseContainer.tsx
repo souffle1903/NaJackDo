2200388e45e5f8cb85bb884e1fb9e7191b98916f
서민수 <minsu9812@naver.com>
2024-09-23 17:32:16 +0900

    [FE] Myfavorite 페이지 api 연결완료
    Former-commit-id: 5b4466690f91d7de5e07d29411384a3a7c034abd

--- client/src/page/library/components/BookcaseContainer.tsx
+++ client/src/page/library/components/BookcaseContainer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9eed5c43af5a8bc15ed96abf9e238819b55900302bd1abc52240f5edc0975439
-size 1279
+oid sha256:644cb4556372c22bb73f5b8fbaac9b6ccc868620e05226a1cdefc4d6afef1ac2
+size 1963
--- client/src/page/library/page/MyFavoritePage.tsx
+++ client/src/page/library/page/MyFavoritePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e602a9ec46e32709135a9d93f238d442760ed53f2aae01ccdffe3edf2fd4c06c
-size 4365
+oid sha256:977639466e7811a125f707eb1dc8b04fa974e1918968d6ed8703e45f9be0e193
+size 3111
