dc98ea29f51bee388eb81c8d87e3fa30a66c8c64
이인준 <dldlswns890@naver.com>
2024-09-23 11:18:34 +0900

    [FEAT] 유효성 검사 api 작성
    Former-commit-id: 3a0cc0cdd5600d64daac6e75a27b46086926fa2c

--- server/src/main/java/com/najackdo/server/domain/survey/repository/SurveyResultRepository.java
+++ server/src/main/java/com/najackdo/server/domain/survey/repository/SurveyResultRepository.java
@@ -1,9 +1,12 @@
 package com.najackdo.server.domain.survey.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.najackdo.server.domain.survey.entity.SurveyResult;
 
 public interface SurveyResultRepository extends JpaRepository<SurveyResult, Long> {
-
+	
+	List<SurveyResult> findByUserId(Long userId);
 }
--- server/src/main/java/com/najackdo/server/domain/user/service/UserService.java
+++ server/src/main/java/com/najackdo/server/domain/user/service/UserService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9216cd4a4f97bb2441c833eaf49e79ef78e3e7a39bd639636a50433e1cca29b
-size 4782
+oid sha256:b316fedd0294d364d16d25d1500f8aa51f5d58f67d3c94d93abbfb170c0b66ae
+size 5153
