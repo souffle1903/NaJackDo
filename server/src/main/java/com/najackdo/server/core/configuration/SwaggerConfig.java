a6afa2ed3922c2c70d0cddfec0e6c69a2f5e14a9
SeongMin Hwang <qq221qq@naver.com>
2024-09-23 10:35:54 +0900

    [DOCS] 스웨거https설정
    Former-commit-id: 4e0d91dbb752e037e50d09f579c5f395e52fb833

--- server/src/main/java/com/najackdo/server/core/configuration/SwaggerConfig.java
+++ server/src/main/java/com/najackdo/server/core/configuration/SwaggerConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e40c446e6c88d7ea812bf9f8aab4e2102f12dc27576e0414edf5d2ad8c6648a9
-size 1072
+oid sha256:a7b10dac3cc0b12d2c10e89598a19e577ccf4f0fdf9aecfbfe7fd07fccd291ae
+size 1297
