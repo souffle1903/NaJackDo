15a8eb7b1d1e71da0a09c8a35e0a24cae38c6e8f
lij0825 <dldlswns890@gmail.com>
2024-09-23 19:36:37 +0900

    [FIX] react-query -> tanstack/react-query
    Former-commit-id: f2331188b3d782dcb551533599c993563e8bd94b

--- client/src/page/login/components/Nickname.tsx
+++ client/src/page/login/components/Nickname.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6fd2ea092d86675eca29ac9f39a1801fee7ee178d325509f9e24f6918573c49
-size 3083
+oid sha256:67da88509c32c636f81420eb51bccca34ee37c5678c287b9aad9e242237a13b0
+size 3009
