127b7ae73bbacacbedcba9ecdb90dfc6aa282b56
Chung <souffle1903@gmail.com>
2024-09-23 09:21:20 +0900

    [FEAT] 장바구니 대여기간 삭제 UI close #18
    Former-commit-id: e86beefa65d3fc91723e140fc2e822828138a555

--- client/src/page/cart/components/CartContainer.tsx
+++ client/src/page/cart/components/CartContainer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89bb2133eefd9ea4de40ba440d48d2649fa05b0d187478c70ab9ae23c3ab8636
-size 4992
+oid sha256:48533d68828fd6e79baa03cbae833d9b77b38e3cdcfa6454beee83bff8f4576e
+size 5037
