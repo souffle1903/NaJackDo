6ae36c66243b6945a9182a588c2d237c8cde3019
lij0825 <dldlswns890@gmail.com>
2024-09-23 22:25:32 +0900

    [FEAT] 장바구니 조회
    Former-commit-id: cfcde6326254fb05907acc193f2e5e5dd3673b1c

--- server/src/main/java/com/najackdo/server/domain/cart/dto/CartData.java
+++ server/src/main/java/com/najackdo/server/domain/cart/dto/CartData.java
@@ -1,25 +1,3 @@
-package com.najackdo.server.domain.cart.dto;
-
-import java.util.List;
-
-import com.najackdo.server.domain.cart.entity.CartItem;
-import com.najackdo.server.domain.user.entity.User;
-
-import lombok.Data;
-
-public class CartData {
-
-	@Data
-	public static class CartInfo {
-
-		User onwer;
-		List<CartItem> cartItems;
-
-		public static CartInfo of(User owner, List<CartItem> cartItems) {
-			CartInfo cartList = new CartInfo();
-			cartList.onwer = owner;
-			cartList.cartItems = cartItems;
-			return cartList;
-		}
-	}
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:70f1febfe4f38845eb7b9362cc35fd938b6a36d13402dd3c3ee02f619cbe24c2
+size 522
