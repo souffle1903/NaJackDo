24c449eeb9388ee7744a6ad3b35987f0a784b064
SeongMin Hwang <qq221qq@naver.com>
2024-09-23 14:20:11 +0900

    [FEAT] S3
    Former-commit-id: cabf52d5f93ae6ea33a86ee46edcecb002741afc

--- server/src/main/java/com/najackdo/server/domain/book/entity/UserBook.java
+++ server/src/main/java/com/najackdo/server/domain/book/entity/UserBook.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:91f3c5e3cfd8a0b59a87ef242c4d371d8388eae8a1b6163f2ba1c77af8ee553c
-size 2099
+oid sha256:fb26b642a1ec0dd97ee4bd869a96fa65583a342012609e9f8f1d9d3ad0dc0ae7
+size 2097
