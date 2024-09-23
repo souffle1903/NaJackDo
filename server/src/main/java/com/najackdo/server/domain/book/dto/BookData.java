7d059ed601bdace02e424a6f91efdc43df9c391b
SeongMin Hwang <qq221qq@naver.com>
2024-09-23 15:05:10 +0900

    [FIX] 책장 검색 DTO 스펙 변경
    Former-commit-id: 5c8782e67c37d33e72f40ed5b35b6b7f85b3c0f1

--- server/src/main/java/com/najackdo/server/domain/book/dto/BookData.java
+++ server/src/main/java/com/najackdo/server/domain/book/dto/BookData.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bf076aae2fee153737b472169af43be547c43b698c1fede4694b002d6d5d6434
-size 1665
+oid sha256:a88be220be6d3e4cee153724179c742e509b1b38c6c08359860d525a225541d8
+size 1808
