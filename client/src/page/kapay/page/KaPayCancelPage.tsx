a1cf9a0f5b9b794e7b4fa0e86e06a8d102d803ab
김도영 <rlaehdud1002@naver.com>
2024-09-23 17:59:23 +0900

    [FIX] login valid state 수정
    Former-commit-id: 595e2afe942402515c9f56677315c3b71df8aca5

--- client/src/page/kapay/page/KaPayCancelPage.tsx
+++ client/src/page/kapay/page/KaPayCancelPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1f8e623babbd57e681ca330a3ef4f4c975c14884a81121e6042ff85ed56bbec7
-size 662
+oid sha256:2990aecbe3e15b5584c21da06c662996d8c3fffeb32063a2e21eda1b3baec567
+size 724
