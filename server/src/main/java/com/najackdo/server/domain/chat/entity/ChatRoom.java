c32514ab71cea6ef239a1fe33ede3920d5c56acc
이인준 <dldlswns890@naver.com>
2024-09-24 15:58:03 +0900

    [FIX] 채팅방 생성 될때 유저들 저장되게 수정
    Former-commit-id: b791163cf972663bf694348b81c2fb95d33c32bc

--- server/src/main/java/com/najackdo/server/domain/chat/entity/ChatRoom.java
+++ server/src/main/java/com/najackdo/server/domain/chat/entity/ChatRoom.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22e37708b36afb184946b525a8a37c3b70c6ff2f0726ac4abd9409377cafd5f5
-size 946
+oid sha256:37703effa3a23a785b27b5cc523742ad0f330adccfd224ef7223c14e7433ba30
+size 900
