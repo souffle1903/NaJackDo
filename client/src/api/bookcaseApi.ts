eb7edbb9d3dc3f88cda7cbba19855030d7c26cca
서민수 <minsu9812@naver.com>
2024-09-24 10:47:05 +0900

    [FE] 책장 조회 API
    Former-commit-id: f654cbaec750ae80408789a752dbf079c0b06f63

--- client/src/api/bookcaseApi.ts
+++ client/src/api/bookcaseApi.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0088274089da912119c87d1c539e33a560afb01b969d0a170615cc15ed7910eb
-size 1439
+oid sha256:edeed4df5549a782c45085f74eb85bde8eef3d6a4380fb090e44bfbc32a5e008
+size 2145
