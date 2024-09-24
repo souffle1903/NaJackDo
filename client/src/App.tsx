53f10ff060374f142486a389c90fc542a3e307cc
DESKTOP-6ABJ64L\SSAFY <poj4639@ajou.ac.kr>
2024-09-24 16:34:58 +0900

    [FEAT] 알람 이벤트처리
    Former-commit-id: df73bda1dcc7fd79fa8640d3e6bf01143e61a8c9

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d02675827e723ff84d02b9282d2dc0a98325525ec031472a3c99493f53a8a7a6
-size 2621
+oid sha256:b92ceab3d9b39c13580e5472b26e140e54caead7b912d7738836d63c0a83609e
+size 2653
--- server/src/main/java/com/najackdo/server/domain/notification/service/NotificationService.java
+++ server/src/main/java/com/najackdo/server/domain/notification/service/NotificationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0b0f5a7a44f2d5c2198bc5e37819b61776c0006d3aec31b0b00c89228967541d
-size 2299
+oid sha256:40d2c318689eeeadb61cbfbf7c290336fe5c189914fd4b89ed91c0db541405c0
+size 3174
