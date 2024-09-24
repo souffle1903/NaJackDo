a90424bedef310af9476d9ab58b1d4c624669942
Chung <souffle1903@gmail.com>
2024-09-24 11:03:36 +0900

    [FIX] 지역 설정 post
    Former-commit-id: a7ae62537f5c7a2a27cb3d21c252e9b609992198

--- client/src/page/location/page/LocationSettingPage.tsx
+++ client/src/page/location/page/LocationSettingPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:475e10ef169503953da8e34d0be9cc1ed7a6c915d1a31553dc0d9f01e8de6916
-size 3189
+oid sha256:8228337043b5c0ba25273145b11f0334d8556400a880b9d7c96985f87ea4c8cf
+size 3247
--- client/src/page/location/page/RangeSettingPage.tsx
+++ client/src/page/location/page/RangeSettingPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d20582c5ecf50f04db06388f88a3aa2170e913b3bb9b64e7a04113d9ff5240a3
-size 4133
+oid sha256:d6f486c8089bcb5ea2256c6c5dc5792b3a1f6bb74cf335f8336005c946537043
+size 4512
