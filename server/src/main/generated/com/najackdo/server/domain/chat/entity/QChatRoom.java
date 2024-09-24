aada0075de43300f7ff82a44f9f2dffd981102be
SeongMin Hwang <qq221qq@naver.com>
2024-09-24 20:42:34 +0900

    [FEAT] 책 조회 시 몽고디비에 데이터 저장
    Former-commit-id: b6227d08e180817250ce164c2e57824991322aff

--- server/src/main/generated/com/najackdo/server/domain/chat/entity/QChatRoom.java
+++ server/src/main/generated/com/najackdo/server/domain/chat/entity/QChatRoom.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5ea62971b9ce6e8a0aeebc6cb6b743aaebe09c6a57963cacd977955855d7c503
-size 2138
+oid sha256:439defbd5964b0db708856ab9de0ccdb0d820913bd169f8c3951a249a08e2e01
+size 2072
