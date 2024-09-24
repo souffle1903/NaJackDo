fbdc3eb885b674c508d5ae12c3b3c17b720ccb8f
서민수 <minsu9812@naver.com>
2024-09-24 10:47:29 +0900

    [FE] 책장 페이지 이름 수정
    Former-commit-id: d5f886a11f8d71d47ddcdaab9689e414484399af

--- client/src/page/library/page/LibraryPage.tsx
+++ client/src/page/library/page/LibraryPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d80017a046f713df4afba786a7c9b40f5a73e5298c22b632f51e0f1a9e33337
-size 5512
+oid sha256:eaf689cc9d982a6a6912039eab8c98188d0644a56c66be32cff3fc3578d7d8c8
+size 5514
