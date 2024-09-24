94963c293df389f013d9443f9b4ca4b2fa4ac843
Chung <souffle1903@gmail.com>
2024-09-24 11:03:36 +0900

    [FIX] 지역 설정 post
    Former-commit-id: ba530a4b72316523348181cd1a504ecd91e9b954

--- client/src/api/locationApi.ts
+++ client/src/api/locationApi.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cb1c59ff94c6fa8f55951f6dd96a82624ff21279aacf8b00475279cd4ccd606
-size 1470
+oid sha256:10fba306306a46e1e4ceb72880bf2b6661b88af5c9a8e67ea10cc0852dde3481
+size 1918
