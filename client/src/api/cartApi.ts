9564236eb0e446d0928af7d27e84b31bed443122
Chung <souffle1903@gmail.com>
2024-09-24 16:06:32 +0900

    [FEAT] 장바구니 api 연결중 close #46
    Former-commit-id: 54c54f6eea3bfa7ab3ad32b3c86d32f96a123019

---
+++ client/src/api/cartApi.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:10626bd569cec3cd2688b045b2090eabc699f90dbcf899492a199ce386a6c9cb
+size 973
--- client/src/page/cart/components/BookRentalInfo.tsx
+++ client/src/page/cart/components/BookRentalInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:940305fede1ad6a390c42e5f556d0e7c622d439bbb2f5a869f2199486b9752e1
-size 1005
+oid sha256:aa38628baba3400058db3eeef46d42c16fa6f2c6e39443c6428cb038d530d07c
+size 1597
