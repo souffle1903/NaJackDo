75f0d91fa05f0dea70e670042fc080e5341fa3e8
서민수 <minsu9812@naver.com>
2024-09-24 09:53:18 +0900

    [FE] type 파일 합치기
    Former-commit-id: 00fb89731e9ac67601476ad6303545f934123be4

--- client/src/api/interstbookApi.ts
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7da52f788e566f042c156b2f2ea13c336633e55f0c166897f91e878c5637b717
-size 1176
--- client/src/page/library/components/BookcaseContainer.tsx
+++ client/src/page/library/components/BookcaseContainer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:644cb4556372c22bb73f5b8fbaac9b6ccc868620e05226a1cdefc4d6afef1ac2
-size 1963
+oid sha256:9550011da680811017c145f2eb8d1f6be6f00b30bf0e170898ef207e180cb74c
+size 1950
