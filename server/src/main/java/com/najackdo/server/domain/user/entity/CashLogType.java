fb257bea64abd09448e4675595774e90ba0374c4
lij0825 <dldlswns890@gmail.com>
2024-09-24 22:12:28 +0900

    [FEAT] rental api 작성
    Former-commit-id: 2c9652efcebf03fb9ca8a055c8be7bd7e9c54eff

--- server/src/main/java/com/najackdo/server/domain/user/entity/CashLogType.java
+++ server/src/main/java/com/najackdo/server/domain/user/entity/CashLogType.java
@@ -1,5 +1,5 @@
 package com.najackdo.server.domain.user.entity;
 
 public enum CashLogType {
-	PAYMENT, WITHDRAW, DEPOSIT, ETC
+	PAYMENT, WITHDRAW, DEPOSIT, RENTAL, ETC
 }
--- server/src/main/java/com/najackdo/server/domain/user/entity/User.java
+++ server/src/main/java/com/najackdo/server/domain/user/entity/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cd7733a4b92fc89726d2d642954bcfb0be5a1b4325f7a32e86461d2e56144852
-size 5008
+oid sha256:bd22cffc502d48cf15ad1575b3cfb6fe0d110dabfc8bd90c294fc8819f5950fa
+size 5036
