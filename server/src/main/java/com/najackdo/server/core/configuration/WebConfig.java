cab5ff64c209ca821d8795ccf427f620d86ed09b
SeongMin Hwang <qq221qq@naver.com>
2024-09-24 20:05:50 +0900

    [FEAT] 책 추천 특정 책 사이트 들어갔을 때
    Former-commit-id: 299a3a76cc868b2fc6d92c3e530129ac4510a254

--- server/src/main/java/com/najackdo/server/core/configuration/WebConfig.java
+++ server/src/main/java/com/najackdo/server/core/configuration/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f7a39675867769525638c6cf9bf8c6846f6d1cd93223bac3ff8fe991e7f8aa1b
-size 644
+oid sha256:2b12c1b54a1fef718d766136f128c3ed004e699d9424824d02c352848319a3aa
+size 828
--- server/src/main/java/com/najackdo/server/domain/book/repository/BookRepository.java
+++ server/src/main/java/com/najackdo/server/domain/book/repository/BookRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:383852f4e5d8804b2bf9c2ffc72cf7ac17c8b79d55e18543959d13e25a711572
-size 781
+oid sha256:3fba4a771e2f674f5c4251e9e70b63129a73320fbaf497260bdde0d793b9232e
+size 883
