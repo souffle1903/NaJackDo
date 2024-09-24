80ddfff4f17ede52dd87a76e3e14f393fd5a58fb
이인준 <dldlswns890@naver.com>
2024-09-24 15:58:03 +0900

    [FIX] 채팅방 생성 될때 유저들 저장되게 수정
    Former-commit-id: 18c9d0c1770e4cee42fdc83a85b94ffb40c8813c

--- server/src/main/java/com/najackdo/server/domain/chat/dto/ChatRoomDTO.java
+++ server/src/main/java/com/najackdo/server/domain/chat/dto/ChatRoomDTO.java
@@ -1,21 +1,3 @@
-package com.najackdo.server.domain.chat.dto;
-
-import com.najackdo.server.domain.chat.entity.ChatRoom;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Getter
-@Setter
-@NoArgsConstructor
-public class ChatRoomDTO {
-
-	private String roomId; // 채팅방 아이디
-	private String roomName; // 채팅방 이름
-
-	public ChatRoomDTO(ChatRoom chatRoom) {
-		this.roomId = chatRoom.getRoomId();
-		this.roomName = chatRoom.getRoomName();
-	}
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b4934edcb0b93589d5cd58fc9073234c65596c1e590502d222d8ad922018b2ff
+size 522
