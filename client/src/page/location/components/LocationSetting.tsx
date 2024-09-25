c3b88a74361a99a356a9bc44520cafe0d63e9880
Chung <souffle1903@gmail.com>
2024-09-25 14:15:55 +0900

    [FIX] 위치설정 컴포넌트화
    Former-commit-id: 826188f4995bbd7098336f4c6c12913d2f8256c7

---
+++ client/src/page/location/components/LocationSetting.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f35c849e713f6f45ee73f5360449cf1e3c72a6386be5b2f78886477921df1a48
+size 4190
--- client/src/page/location/page/LocationSettingPage.tsx
+++ client/src/page/location/page/LocationSettingPage.tsx
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e83c903385a006f3935f287387ec264252985279b21c56db5ae0d18b0a2a10a0
-size 4113
+import LocationSetting from "../components/LocationSetting";
+
+const LocationSettingPage = () => {
+  return (
+    <div>
+      <LocationSetting nextUrl="/location/range" />
+    </div>
+  );
+};
+
+export default LocationSettingPage;
+
