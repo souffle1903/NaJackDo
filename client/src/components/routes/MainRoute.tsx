d308da7d7a33465f85f62fca76692d2d91d68e80
서민수 <minsu9812@naver.com>
2024-09-25 11:32:57 +0900

    [FE] 타인 책장 조회 api 연결
    Former-commit-id: eafae906ee1b29a8360a8ca696f08e37c0cff3df

--- client/src/components/routes/MainRoute.tsx
+++ client/src/components/routes/MainRoute.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:407b76a7bc8c9ecbba95c20daa9073f7fd5e27c6a65cd44679d7f007097f997f
-size 4511
+oid sha256:db8aebf9dbcb02ed8c9a8ccc3f9b7d5d123729058d9eb1cd36ea878562cf987e
+size 4660
--- client/src/page/library/components/BookGrid.tsx
+++ client/src/page/library/components/BookGrid.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c136f95cc1dd05ddd3cf70f93c68617e32559e06d35f80071755f27ab9e347a1
-size 4418
+oid sha256:f59daa3b7007c398e0d3e2c2668a3bbda2271498cff3bd4df01f58715b6502f7
+size 4441
