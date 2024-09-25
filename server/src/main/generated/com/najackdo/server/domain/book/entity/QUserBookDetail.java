d21d2ac7fa0e35e83ac61a28539664128f821899
이인준 <dldlswns890@naver.com>
2024-09-25 15:00:07 +0900

    [FIX] 도서 상세 엔티티 수정
    Former-commit-id: 1542cfbb7a1b7e0d31999e7c3519e36474b374b6

--- server/src/main/generated/com/najackdo/server/domain/book/entity/QUserBookDetail.java
+++ server/src/main/generated/com/najackdo/server/domain/book/entity/QUserBookDetail.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7daa4e225086380fed4cdbb9e6cb51056f825fa069d899b13a6b539bfa3d9ad
-size 2710
+oid sha256:3955d2e0ef161aa85e9ef2c5e7684dbbfc2a8b2c86f19dbb6e25a4b7cd7622d0
+size 2376
