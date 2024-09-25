75fa64068b7a8e54c2e505306f611fc2dbf2afb7
Chung <souffle1903@gmail.com>
2024-09-25 14:15:55 +0900

    [FIX] 위치설정 컴포넌트화
    Former-commit-id: d5d35d465660b50b2b85e4f2ef12b5ffbce01938

--- client/src/page/location/page/RangeSettingPage.tsx
+++ client/src/page/location/page/RangeSettingPage.tsx
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d6f486c8089bcb5ea2256c6c5dc5792b3a1f6bb74cf335f8336005c946537043
-size 4512
+import RangeSetting from "../components/RangeSetting";
+
+const RangeSettingPage = () => {
+  return (
+    <div>
+      <RangeSetting />
+    </div>
+  );
+};
+
+export default RangeSettingPage;
+
