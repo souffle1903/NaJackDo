8ef3b4889173fd45389cb3ca1f5daca09da7c9ce
ParkBeomSu <beomsu4639@gmail.com>
2024-09-25 10:43:32 +0900

    [FIX] najackdo-notice.json 위치 변경
    Former-commit-id: 6df8613e44d5d0c16ce0a47d7d95e19a488f6d6d

--- server/src/main/java/com/najackdo/server/core/configuration/FCMConfig.java
+++ server/src/main/java/com/najackdo/server/core/configuration/FCMConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57a9eb040f76ee6d149bbc5c44a469feb4c71661cffc71fd4475a0d04d9f6f9d
-size 1462
+oid sha256:9ea14505592cb68d9fa8abb90c894433d2380aeedb42a538c6a0d40f880119bc
+size 1453
