c9e31d791c6922d6cc6ee8636399c6014daffe9d
SeongMin Hwang <qq221qq@naver.com>
2024-09-25 15:39:26 +0900

    [MERGE]
    Former-commit-id: 7b9023b61014f73b8835a8bc2009ff48531702f2

---
+++ server/src/main/resources/recommendation/app/dao.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:afab0bba157448d5be64012f03f97369391056d461fdb2e894d64688980159a7
+size 2185
---
+++ server/src/main/resources/recommendation/app/quality_inspection/yolo.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:80f1f2f4c118f497c41138b1115ae0ecff7a642967364c298c2c2e3afb0ed514
+size 536
