fb2b7a417cab7582bd65376696f8853eaea23ce9
서민수 <minsu9812@naver.com>
2024-09-25 17:46:43 +0900

    [FEAT] 프로필 페이지 API 연결
    Former-commit-id: 80e7b1754ed8a13db7958fe4d14fa709e2ee12b7

--- client/src/page/profile/components/MyLeaf.tsx
+++ client/src/page/profile/components/MyLeaf.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb8e2ee18d04598b056cafd652f5d3dea08c9f617d73350439a1d36ad99a8867
-size 1040
+oid sha256:4d684d39f6b854bad8dd5fc8b3e17dc8e7bcf008e7b1a05c7ee2b9d82d732cc8
+size 1785
