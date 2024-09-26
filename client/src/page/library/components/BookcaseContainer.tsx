a80fd68b346d2baad7e9423fe49daee64b951eb2
서민수 <minsu9812@naver.com>
2024-09-26 17:53:54 +0900

    [FE] 타인 프로필 조회 api 연결
    Former-commit-id: 21da9e644bf6feae34bf90f9da5e1aedf88b200e

--- client/src/page/library/components/BookcaseContainer.tsx
+++ client/src/page/library/components/BookcaseContainer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:200997d159fc19ba21da3fd550f9fe9dd081059726e5895b860412a29806a5e5
-size 1862
+oid sha256:148562255fe20a20647a05f3606e29ed9ecef086784572b196bd5cac4b8ea62c
+size 1855
