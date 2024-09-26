2e8e56d8fcd42c183061d4c6b5abab3da2e9649c
SeongMin Hwang <qq221qq@naver.com>
2024-09-26 17:23:35 +0900

    [MERGE]
    Former-commit-id: 3d96855212f0454c61fffbaa03dcc7ff38af044f

--- server/src/main/java/com/najackdo/server/domain/chat/dto/ChatRoomDTO.java
+++ server/src/main/java/com/najackdo/server/domain/chat/dto/ChatRoomDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4934edcb0b93589d5cd58fc9073234c65596c1e590502d222d8ad922018b2ff
-size 522
+oid sha256:21f8751f8f86a651f03defc457ee37bc6ea8a06d89edf86aa68439f14b2bb692
+size 620
