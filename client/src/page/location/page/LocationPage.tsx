89b3b3ce43773b6965c41fc4e1809f9f4804016c
서민수 <minsu9812@naver.com>
2024-09-26 11:44:02 +0900

    [FE] 팔로우 기능 수정
    Former-commit-id: af439ee449e90bd1ab10303f1b69d0d2a44569ac

--- client/src/page/location/page/LocationPage.tsx
+++ client/src/page/location/page/LocationPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d7271d85a31e3e68e12d3ca22648d2f90bb1b14a8eb22eac091b3e53c9e6297
-size 3386
+oid sha256:a0e39f58e666bf9191fa09019e73a10039aa3d6b561fb512e3cedb6aafa353e2
+size 3397
