792028b43893b024986e46b897a64c08f1cae51e
이인준 <dldlswns890@naver.com>
2024-09-26 16:51:37 +0900

    [FIX] InfoResponse id -> userId
    Former-commit-id: 2487baa82b87ce5df5baa6ad62697ad8124fb3fc

--- server/src/main/java/com/najackdo/server/domain/user/dto/UserData.java
+++ server/src/main/java/com/najackdo/server/domain/user/dto/UserData.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37c5e5b077dd63b0330edc99c955b4ca4f571202446ea5bdb6147ddc8759e6a8
-size 3351
+oid sha256:445f14d26f428d4369bbb5d092e4fb7b139c35efb6f68314a5b5be4e09f968a0
+size 3359
