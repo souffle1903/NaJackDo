ec95e30c04973cb99b4b9f42e29a26120ace80bf
SeongMin Hwang <qq221qq@naver.com>
2024-09-26 10:34:57 +0900

    [FEAT] 품질 검사 기능
    Former-commit-id: 5f08c8a81a18607aa112bf5fb0a006c3387483e4

--- server/src/main/java/com/najackdo/server/domain/book/controller/BookController.java
+++ server/src/main/java/com/najackdo/server/domain/book/controller/BookController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:558d79eb01b2d1c00eddbee70a5b3b188b0654ab36e49c69f6cda23fabf9282c
-size 5093
+oid sha256:7e0cc1ebd4bf903e0a2e9c379569038dad9f55e33e055dcf5350c3ebbb35f473
+size 5094
--- server/src/main/resources/recommendation/app/dao.py
+++ server/src/main/resources/recommendation/app/dao.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afab0bba157448d5be64012f03f97369391056d461fdb2e894d64688980159a7
-size 2185
+oid sha256:f7981345e5c398fac7f807f616ed451e18f0873bae106ddfa42d0328bc2690ba
+size 2172
