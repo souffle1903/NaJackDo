63eae219e4f936e03cad0eabb410c9dfa245a61a
이인준 <dldlswns890@naver.com>
2024-09-26 15:34:54 +0900

    [FEAT] 근처 책장 페이징 처리
    Former-commit-id: 5e8dd160fd2e39fb93e0557a4fc35438e757cc18

--- server/src/main/java/com/najackdo/server/domain/book/controller/BookController.java
+++ server/src/main/java/com/najackdo/server/domain/book/controller/BookController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:deaf9c405c77e29c037c2dcaa70573d7a81e832f9d68b995fceecd76e4ba65e8
-size 5633
+oid sha256:8e5897559410805d03cb55102f62cba38753b15da9761e2dd05c0d93fc5a4383
+size 5756
