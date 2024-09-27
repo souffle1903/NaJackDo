48da7cd964009a023cc8e11d14de15bc33e04aba
서민수 <minsu9812@naver.com>
2024-09-27 15:00:42 +0900

    [FIX] 에러 수정
    Former-commit-id: c3a2b4268317519b193a8052a6e200b30e05604d

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa46f0d6a824508881a3dd04ff63195d70c5eda5fd38d4ad3d9ae33f04a672d4
-size 4798
+oid sha256:fa58bb46ca05f3d7934577f904a24fa8ba74512eb6ec74aa537f815aa4ccc5ec
+size 4850
