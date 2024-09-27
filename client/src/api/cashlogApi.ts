0216aa30611043a6828a79f0e353a78d6a70c677
Chung <souffle1903@gmail.com>
2024-09-27 14:04:14 +0900

    [FEAT] 책잎 사용 내역 API 연결 close #55
    Former-commit-id: 3e7e65cd0f1b7342d3c166ce2b318e4ed1e78696

---
+++ client/src/api/cashlogApi.ts
@@ -0,0 +1,17 @@
+import { BaseResponse } from "atoms/Base.type";
+import { ICashLog } from "atoms/CashLog.type";
+import instance from "./clientApi";
+
+// 책잎 사용 내역 조회
+export const getCashLog = async (): Promise<ICashLog[]> => {
+  const {
+    data: { success, data },
+  } = await instance.get<BaseResponse<ICashLog[]>>("/user/cashlog");
+
+  if (!success) {
+    throw new Error("책잎 사용 내역 조회 실패");
+  }
+
+  // console.log("getCashLog called", data);
+  return data;
+};
