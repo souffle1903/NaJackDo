fa967f667b44709f994fa043277d762a10f69f23
SeongMin Hwang <qq221qq@naver.com>
2024-09-27 17:14:43 +0900

    으으으아아아아악
    Former-commit-id: 77affbf7fdea1362feb9fdd4b39b5810fb1ea84b

--- server/src/main/resources/recommendation/app/main.py
+++ server/src/main/resources/recommendation/app/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17ae883d8ec05537ff5273875ffa513d14c925d498bba8f0955da5d0403274c3
-size 4636
+oid sha256:e85b757e2d18a73c20816f300901e42c42e867100843d7a987fd7dd08e6bc1e2
+size 4533
