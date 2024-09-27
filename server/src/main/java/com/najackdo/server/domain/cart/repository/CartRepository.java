df71077b919d2f603aa53affd4cb4ee247fe7ac7
SeongMin Hwang <qq221qq@naver.com>
2024-09-27 13:45:20 +0900

    [FEAT] 채팅방 조회시 대표사진 추가
    Former-commit-id: 307e420cfd15984b04f3dd1cde3f5968ab212a93

--- server/src/main/java/com/najackdo/server/domain/cart/repository/CartRepository.java
+++ server/src/main/java/com/najackdo/server/domain/cart/repository/CartRepository.java
@@ -1,6 +1,7 @@
 package com.najackdo.server.domain.cart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.najackdo.server.domain.cart.entity.Cart;
 
