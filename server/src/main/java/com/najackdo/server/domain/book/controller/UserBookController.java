53be159047eb1be18cbf657128116accb12595b1
이인준 <dldlswns890@naver.com>
2024-09-27 12:00:52 +0900

    [FIX] 일일 대여료 업데이트 api
    Former-commit-id: 58679e0ae0b79f925473c7ddaab281639ac1b2be

--- server/src/main/java/com/najackdo/server/domain/book/controller/UserBookController.java
+++ server/src/main/java/com/najackdo/server/domain/book/controller/UserBookController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89b0ce070ce8c0e12a6cf2d2ae1974c274d1f7935d93f3649660532e387afdab
-size 1305
+oid sha256:9272bf68c9a93d43d63140c8d6a7c6bd1ebd30328b7fd98979abffd95c7d9487
+size 2018
