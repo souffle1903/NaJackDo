31fc561d4bcde9239a999d1df10b821789325505
서민수 <minsu9812@naver.com>
2024-09-27 17:51:56 +0900

    [FEAT] 도서등록 ai인증가이드 ui
    Former-commit-id: 158dd6c425649fb8b2e738b5456da0ab46436ac3

--- client/src/page/bookapply/page/AICheckPage.tsx
+++ client/src/page/bookapply/page/AICheckPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4c016f19ceaa266b7f1f906909ea62cf94fb523039b32e4431009729ef2083cb
-size 1667
+oid sha256:17fb2e86bc389c70f2afe76e89adfa5dde43c2a0d2eb8f66536369f42372246a
+size 3575
