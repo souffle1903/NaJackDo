3700f844750522d95ede2007df25126226699737
김도영 <rlaehdud1002@naver.com>
2024-09-29 21:48:00 +0900

    [HOTFIX] 배열 문제 해결
    Former-commit-id: 44bc383a861c73b815c0f4c56ac87816018fcd0d

--- server/src/main/java/com/najackdo/server/domain/chat/dto/ChatRoomData.java
+++ server/src/main/java/com/najackdo/server/domain/chat/dto/ChatRoomData.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:053cdebb53d535cfb58da8fb161481484f84912e4bbe5a4905dbbfeb69701b58
-size 2555
+oid sha256:8e6de584b943203dee97d4f4edb911b231641f8034ecc9c595d3c481a39e92d6
+size 2557
