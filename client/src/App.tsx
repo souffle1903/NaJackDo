2727dd731f3485ff03c4847d8c1efc534bb17f41
김도영 <rlaehdud1002@naver.com>
2024-09-29 21:32:41 +0900

    [HOTFIX] 메시지가 없을 경우 처리
    Former-commit-id: 7f2fa7d99290078f8a115b80f70e3250f312d326

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa58bb46ca05f3d7934577f904a24fa8ba74512eb6ec74aa537f815aa4ccc5ec
-size 4850
+oid sha256:e0e86040f2139dd3392e764e8f3401c8ba2005b2c0e40bd92630715fe90ae425
+size 4943
--- client/src/page/location/components/LocationSetting.tsx
+++ client/src/page/location/components/LocationSetting.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd8f4a88ddbabb991a60fda3c4443a68a78bd3ee07b95a697fb9f56f1bf39bc1
-size 4148
+oid sha256:be896d2d0701d976f163b56cb5e30603daf322633b664826c0a13d3a54ae946b
+size 4052
