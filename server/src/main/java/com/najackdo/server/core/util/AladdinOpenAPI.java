e5bce6a54bf397f2a28c3784d56c03b22f31f991
ParkBeomSu <beomsu4639@gmail.com>
2024-09-30 11:31:32 +0900

    [FEAT] 책 데이터 추가 수집
    Former-commit-id: 6bfc0607cd1ca623cecd02fc89867e7880842dec

--- server/src/main/java/com/najackdo/server/core/util/AladdinOpenAPI.java
+++ server/src/main/java/com/najackdo/server/core/util/AladdinOpenAPI.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1eb9b9bf84897b7eecac92d8876b4b4e58fe10e408163f386179d9bee93a9698
-size 9760
+oid sha256:7a7bd54fbd354603117d0adcf03155796e637b9741ec14ccb6d961ca00591818
+size 9813
