b2bed2b196f52a4a4c2a0ad1cc4083c8be4d8351
Chung <souffle1903@gmail.com>
2024-09-30 18:01:10 +0900

    [FEAT] 상세페이지 연결
    Former-commit-id: 824bc4898442d8b1d5291eca182f844733bcdedd

--- client/src/page/library/components/SellerInfo.tsx
+++ client/src/page/library/components/SellerInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d039040ae0d6dbef581489360363e7457f5c0c7127f3c30f9bb5b7c44c9f4a85
-size 1219
+oid sha256:edbdd215e7fb9d901e30bf215571ecbaf1c53ee1c9d01807abd744c20760daab
+size 1525
--- client/src/page/library/page/RentalBookDetailPage.tsx
+++ client/src/page/library/page/RentalBookDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89fda8a46b701adc2067792644a796126e36b3be1e616b1df5ec6bec3e9cc745
-size 874
+oid sha256:3429c5aa5bb538f1ce8b995b6514b9f8caea52eb55164fbacfd0c3afd55a8c5b
+size 2646
