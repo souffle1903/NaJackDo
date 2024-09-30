91e7233465971cad01ad59cfd856cf54c3a9d389
SeongMin Hwang <qq221qq@naver.com>
2024-09-30 10:33:54 +0900

    [FIX] 책 검색 시 즐찾 여부 추가
    Former-commit-id: 1da1c3e3ca87581dcc204c3d3ddb8197dbdd35ba

--- server/src/main/java/com/najackdo/server/domain/recommendation/service/RecommendationService.java
+++ server/src/main/java/com/najackdo/server/domain/recommendation/service/RecommendationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ce5647a168350d3e8d9fcc2ce9bbf79c5bf684636ef6ed4eabf2c7b4e77694a
-size 3059
+oid sha256:cc72dca118ec86de204503c5d3c28727259787c2ffbfd8282f29b050dcc7cf9b
+size 3027
