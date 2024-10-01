2fd2840746559ca91f0b402ae65b75aa07bc9df2
김도영 <rlaehdud1002@naver.com>
2024-10-01 14:16:22 +0900

    [FIX] merge error 해결
    Former-commit-id: 7078cfa4bd8053eb02c65153333fdd40b5d299df

--- client/src/page/library/page/RentalBookDetailPage.tsx
+++ client/src/page/library/page/RentalBookDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f13b1b00bdff86827d580abec236e5df56c0f9cc68e28370751065e8f7edc682
-size 2668
+oid sha256:2825c54803f528cfefb32e31173b84d7486305c14068b62715b6230b609d5c6b
+size 2604
