8ac83c236ad7a25f41ec8a9c8316d73037bb2b42
Seo Minsu <seominsu@Soo-MacBookPro.local>
2024-10-01 17:57:40 +0900

    [FEAT] 제목 한글로 변경
    Former-commit-id: df39f99fa5dba0c6c09977ad560d84893241aebc

--- client/src/page/library/page/MyFavoritePage.tsx
+++ client/src/page/library/page/MyFavoritePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fdee6612a56013411600601fd7a9003581bfd0ad94b246cc83d382da52cec871
-size 3157
+oid sha256:573462c8af434394c68a744192b0dca5da298791f86ed79404f4dd021ab20a2c
+size 3172
