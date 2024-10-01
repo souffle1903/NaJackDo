63ef3870b6c809c541ddac8c3d108fc941c99fe2
SeongMin Hwang <qq221qq@naver.com>
2024-10-01 16:19:28 +0900

    [FIX] 이것저것 수정 카트 상태 추가
    Former-commit-id: e668bbd72d8349c728210c893b85994d53a2bf65

--- server/src/main/resources/recommendation/app/main.py
+++ server/src/main/resources/recommendation/app/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f87fad52be47db9a610234483e08418f306eb2f9e013a4dea3cf583b9ecf644d
-size 6759
+oid sha256:0e91c341526e18f47fa1f535f9a91fe8f9a6a3b7257ed0ba2430f1b99f01e84d
+size 6930
