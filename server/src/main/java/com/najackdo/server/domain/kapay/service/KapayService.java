5070d5b75730e81ad85b88f95923d1eeb4922341
lij0825 <dldlswns890@gmail.com>
2024-10-01 21:50:10 +0900

    [FIX] test용 코드 수정
    Former-commit-id: 3e2be252151cea5b345da300de10470f442fa4de

--- server/src/main/java/com/najackdo/server/domain/kapay/service/KapayService.java
+++ server/src/main/java/com/najackdo/server/domain/kapay/service/KapayService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a306bc79c499fffb6eff77c6579a6431d692da415390a67298277d6bcc3495fa
-size 5114
+oid sha256:66700f704895254dfc7c085f5ecba6841643fb3a44224c7a48782f12001bd289
+size 5061
