01004eeda81eb2ff5f25dad15dec0bd3f390b670
김도영 <rlaehdud1002@naver.com>
2024-10-01 14:18:58 +0900

    [FIX] route error 수정
    Former-commit-id: 75f07838c0a1f74f2839f6f2c769b93d1f1621e0

--- client/src/components/routes/ProfileRoute.tsx
+++ client/src/components/routes/ProfileRoute.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9821f5f42940030a1a90fa5cf50cac0150dc86b692eb1f16b75b749ea20df3f2
-size 860
+oid sha256:2207d54c79a455737653d29cf25963929dd6d61197469fd272ed2f585efefb9d
+size 821
