978f3b6aa60829623b50095973530294063e8b89
Seo Minsu <seominsu@Soo-MacBookPro.local>
2024-10-01 17:59:05 +0900

    [FEAT] AI 인증 연결 완료
    Former-commit-id: 92035592d58b090da9521b40b72f1244ccd4d1ac

--- client/src/page/bookapply/page/AICheckResultPage.tsx
+++ client/src/page/bookapply/page/AICheckResultPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efa93192ca5de422c8a5c06e13bbec0ac04efd36855cea6ae2da40ab26245a5c
-size 1456
+oid sha256:4c73f9f1cf09ca4a6cf0820444b3c4f0691bf99df8cbbad3a0b9ca08ccba4777
+size 3956
