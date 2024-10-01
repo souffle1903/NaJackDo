67ed7a858be2317f9e8382850a71ed145d2685a6
chung <souffle1903@gmail.com>
2024-10-01 14:04:31 +0900

    [FIX] 대여 도서 상세 페이지
    Former-commit-id: 44905d3aa14ae48565216c4bf17dbbc8689d8fe2

--- client/src/page/library/page/RentalBookDetailPage.tsx
+++ client/src/page/library/page/RentalBookDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3429c5aa5bb538f1ce8b995b6514b9f8caea52eb55164fbacfd0c3afd55a8c5b
-size 2646
+oid sha256:4da3fe444ed23646a3b671148f56ae87353d5d7cdb69ab56a5a4c79a179504aa
+size 2675
