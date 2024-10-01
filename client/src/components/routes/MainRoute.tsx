2a8f86c7ef13049e6a017c8eccc1ccf67e12eb93
김도영 <rlaehdud1002@naver.com>
2024-10-01 11:56:48 +0900

    [FEAT] userInfo session 저장, chatting 진행중
    Former-commit-id: 7ce5ecb95be5fc4806582141f6f943054d5fba9a

--- client/src/components/routes/MainRoute.tsx
+++ client/src/components/routes/MainRoute.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:228f0ce173c191620261b375a4034939e32067639c1d8cf4a7ab023fce55b1fd
-size 4916
+oid sha256:78deee0c222f264919b9e886f0b2deca3044e6a0553c11db1e94f77ebd3e46b2
+size 4915
---
+++ client/src/page/chatting/components/ChatRoomList.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f0db396da8c2b794c78cae0746fbf2d42de65f2b01cdc5b6b65b47da3bebda9f
+size 970
