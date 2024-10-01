98421b4d1a6963f245b986ad159fd759328ee5a7
SeongMin Hwang <qq221qq@naver.com>
2024-10-01 16:56:09 +0900

    [FIX] 웹소켓 CORS
    Former-commit-id: 49ad33b5f7da24966b9696176312c1dd497f8c9f

--- server/src/main/java/com/najackdo/server/core/configuration/WebSocketConfig.java
+++ server/src/main/java/com/najackdo/server/core/configuration/WebSocketConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3cf2a944e2235e6fd6d962069356940fe50daafffbb7ccd73c1780408291267c
-size 1844
+oid sha256:3185f5f2bb8b838a3b6ab452ac10f1f5994170cf20c6e9e4bc221189a6aaf9d7
+size 1904
