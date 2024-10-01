3bfc9781cc517a4b0da90a79fc82cc914b73530f
김도영 <rlaehdud1002@naver.com>
2024-10-01 11:56:48 +0900

    [FEAT] userInfo session 저장, chatting 진행중
    Former-commit-id: b18fc64db8b6036ad4cfe22e4497ef42d81e3e63

--- client/src/api/clientApi.ts
+++ client/src/api/clientApi.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10a007abbec6840f84e4a770978838ff9953b4bcd1e24e9cbcd5a6b25d90b728
-size 2379
+oid sha256:71da8221b9d15a874c7cb47e5956495960c544bed3ae527ceb3ce9d62fd24e39
+size 2403
---
+++ client/src/store/useUserStore.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f8d59dff85ee3bd38bb4c12fecd14023a8b55393bd7663dacd54b456b27be0b0
+size 855
