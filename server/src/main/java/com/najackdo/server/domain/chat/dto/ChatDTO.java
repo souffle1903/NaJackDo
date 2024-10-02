de8fd034ff0b9da3d396c57d0089e52b21b0bc83
SeongMin Hwang <qq221qq@naver.com>
2024-10-02 14:39:05 +0900

    [FIX] 채팅 메시지 타입 추가
    Former-commit-id: f319b242e9c83f375731be8ba920b9c04f7f6280

--- server/src/main/java/com/najackdo/server/domain/chat/dto/ChatDTO.java
+++ server/src/main/java/com/najackdo/server/domain/chat/dto/ChatDTO.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57ce4a403d0429e88cbcb119e0208c4de314753c4c0a4a954acd062d4aa196de
-size 570
+oid sha256:18e4de4b6cecd185134c9ff8e3bf71921851ad7c371a115f60a0813cff22964c
+size 618
