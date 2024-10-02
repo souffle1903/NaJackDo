e87a0006a3f579dbfa49627a73e19bea138eb52b
Chung <souffle1903@gmail.com>
2024-10-02 17:47:47 +0900

    [FIX] 사용자 리뷰 남기기
    Former-commit-id: 559bfe4346c4cc7a111e5ae73437ffed6655e6a0

--- client/src/page/chatting/page/ReviewPage.tsx
+++ client/src/page/chatting/page/ReviewPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18049ccc57577f6f8b04c808285804e5d0c1978c5ad26183f199bed40d83048d
-size 4456
+oid sha256:51868f03b4d088eb6c5c727171c87c9d65253a6454e5a0ed31b82501fad81fff
+size 4683
