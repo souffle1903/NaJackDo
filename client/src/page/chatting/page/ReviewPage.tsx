d48d97f4798df93c69f31c7fb17c32e63e4c1ed5
Chung <souffle1903@gmail.com>
2024-10-02 17:46:23 +0900

    [FEAT] 사용자 리뷰 남기기 ing
    Former-commit-id: 7ba51ddc496cbc9ded81e3803be020cb2ef6dcbe

--- client/src/page/chatting/page/ReviewPage.tsx
+++ client/src/page/chatting/page/ReviewPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6d68a3a3e918078de6c0ff0cc4bde754d68a39ec73cb1ceca13eb3723c36644
-size 3591
+oid sha256:18049ccc57577f6f8b04c808285804e5d0c1978c5ad26183f199bed40d83048d
+size 4456
