aec69099468ff2530b309195014a27eb708e6473
Chung <souffle1903@gmail.com>
2024-10-02 10:29:29 +0900

    [FIX] 체류시간 수정
    Former-commit-id: 701e1c4fc160cef00adf38351fc70aa16b0edd2c

--- client/src/page/library/page/BookDetailPage.tsx
+++ client/src/page/library/page/BookDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afd540441205bbc46f75b85f2aae6ca6518cb6162c2722350bb2743975735188
-size 3170
+oid sha256:7f95fd6975512da42e44f2b22debd172deacef0186b067f05ccb239e781bf950
+size 3133
