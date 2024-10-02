f93267d627ce6418f5213b3c50dfe3bc6e8a841d
SeongMin Hwang <qq221qq@naver.com>
2024-10-02 16:47:34 +0900

    [FEAT] 특정 책 주변에서 빌릴 수 있는 목록 조회
    Former-commit-id: 59cdf169bcea5213b07a79b1f974195acc6da5d2

--- server/src/main/java/com/najackdo/server/domain/book/controller/BookController.java
+++ server/src/main/java/com/najackdo/server/domain/book/controller/BookController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94e9716d1d92f921d08091cb42ca3b0a4ec693cb75d92d7f835e026501d62854
-size 6336
+oid sha256:ad4c8df5fa7c2ec9842cc8de56245de24cac4e37c1d5f25b6e642178f6bccb8a
+size 5785
