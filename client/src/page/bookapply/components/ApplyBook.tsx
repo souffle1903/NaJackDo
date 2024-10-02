c6e579111dbf0986a48f94af218fc3cbedb4677d
이인준 <dldlswns890@naver.com>
2024-10-02 16:41:00 +0900

    [FIX] img_base_url
    Former-commit-id: af1bb2ad3379fbceba7ce6b02476b920fb025da0

--- client/src/page/bookapply/components/ApplyBook.tsx
+++ client/src/page/bookapply/components/ApplyBook.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c82fd17afa5961bc74c8fdf631667e711d82d979a56ff2b54eab15d4b4ddc54
-size 719
+oid sha256:afa49b62522c255669e6e1030dd65b1c1753c91220be433a33378e904c37933a
+size 698
--- client/src/page/chatting/page/ReviewPage.tsx
+++ client/src/page/chatting/page/ReviewPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbb645d05e16cc9aeb14dec69c777507021781cce9a6be677ebb981c30a671b8
-size 3722
+oid sha256:fcd1bfddebf76be8b6c49b6819e900bffc9af25ff431ed40b7ef32cf547498f5
+size 3740
