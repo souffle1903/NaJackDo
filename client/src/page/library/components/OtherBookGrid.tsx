5c4ee855299cbb4cf2adba350e9ce8ca4f6bdae4
서민수 <minsu9812@naver.com>
2024-10-02 17:44:47 +0900

    [FEAT] 남의 책장에서 책 선택해서 담기 완료
    Former-commit-id: 2209710e3f11ec3c58848516faa50f089bcf3c5c

--- client/src/page/library/components/OtherBookGrid.tsx
+++ client/src/page/library/components/OtherBookGrid.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9042f26c179bde198f9f4856451d70f0bf91457d63631234a3ab47050ffae92e
-size 4194
+oid sha256:0be86a116e9442b852374a494e6061caefd3ece477c7794c654d3f13a30975bb
+size 6076
