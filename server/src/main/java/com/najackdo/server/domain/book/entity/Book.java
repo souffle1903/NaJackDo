36e83fdba639c7143f8f51a32bde3f4365cfb11f
SeongMin Hwang <qq221qq@naver.com>
2024-10-02 11:06:34 +0900

    으으ㅏ악
    Former-commit-id: a634d02201578e936f2afb6a30b8f3253317020a

--- server/src/main/java/com/najackdo/server/domain/book/entity/Book.java
+++ server/src/main/java/com/najackdo/server/domain/book/entity/Book.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddd504e8b7410eb2a10033d85d9db5d97cd427b48d17ca47cbd19c758030f22c
-size 1810
+oid sha256:4cbb1ce49a361a674e25ae4617ae94436427c7d2f5f6790882179edae3807b7e
+size 1837
