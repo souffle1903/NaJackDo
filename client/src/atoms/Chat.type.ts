044589eb206835b04de537b18e185b8198d28276
김도영 <rlaehdud1002@naver.com>
2024-10-03 17:18:44 +0900

    [FEAT] 상대방 버튼 실시간 활성화
    Former-commit-id: f693b4d3b0572dc431deedb67d220dfdb9a404e6

--- client/src/atoms/Chat.type.ts
+++ client/src/atoms/Chat.type.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30b6a3acf161e8930249024dbe15a409b9819633f21f3f3d19cdb3d86b9525eb
-size 687
+oid sha256:717dcd87ac7932910b6f2dce1200c8e7737d230640698f5e757e3d69e6a1cc39
+size 733
