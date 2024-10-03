729bb78993bdce7cd88fcdcc9744319f5500457d
SeongMin Hwang <qq221qq@naver.com>
2024-10-03 16:26:42 +0900

    [FIX] 채팅인덱스 에러 개선
    Former-commit-id: 04b9bae26c6b8af645a9dbd7d2c4aeb60462d4a5

--- server/src/main/java/com/najackdo/server/domain/chat/service/ChatService.java
+++ server/src/main/java/com/najackdo/server/domain/chat/service/ChatService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b9cea522a1d0290c11b234ac15f1039b9e029d00563e9cacb49d3c1221fac4a0
-size 2967
+oid sha256:d70c3d512f39cafcb5713dbc48b0ee5b3daecc5f04afc60e3e8501169537576a
+size 2823
