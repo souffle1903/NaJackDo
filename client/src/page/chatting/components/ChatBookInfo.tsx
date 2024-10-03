34da074a8309abec980a9199d57e25617a7f6d1c
김도영 <rlaehdud1002@naver.com>
2024-10-03 18:07:29 +0900

    [FEAT] 후기 rentalId
    Former-commit-id: f8320033910c363b7a1ca1058d5affb47619a0e2

--- client/src/page/chatting/components/ChatBookInfo.tsx
+++ client/src/page/chatting/components/ChatBookInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5c64685a695e8686589c1ccea12c8f19a543888e7f82a527775abeea0e77fe7
-size 6172
+oid sha256:cfad7c95ba06aae26ad668a299b1ed38db7dd49838b075a85022ffd838605358
+size 6202
