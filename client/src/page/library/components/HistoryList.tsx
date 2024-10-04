50dadae010b7387d4ca7662293ff43a3c0200129
김도영 <rlaehdud1002@naver.com>
2024-10-04 16:40:49 +0900

    [MERGE]
    Former-commit-id: 73cb480592fd63fb7de609ec2c44de60a19170aa

--- client/src/page/library/components/HistoryList.tsx
+++ client/src/page/library/components/HistoryList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:896cccb577e2b18deeb57a6ae81cf6feb0a10e8108bed4563a16e47dc794bd88
-size 2384
+oid sha256:07282b705c5af7830163c3be88a1f943261c02214898ba1c3fa00f8c96e0c108
+size 2547
