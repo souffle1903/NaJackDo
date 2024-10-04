fbf14b2419b98adca1ede2404e17c08e38241bd1
서민수 <minsu9812@naver.com>
2024-10-04 11:06:26 +0900

    [FEAT] 로딩창 수정
    Former-commit-id: e0f17ad6c072f03cb349a468b146a7602d248842

--- client/src/page/library/page/MyBookCasePage.tsx
+++ client/src/page/library/page/MyBookCasePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:092ff6191be05c52ceed56d8adc03cb323b682957477b62690730e5a686770a8
-size 3224
+oid sha256:65aa4ca978aa9c93997f81a3cbaccea50b764b3dbb0cc28c5908c6c1220ff01b
+size 3260
