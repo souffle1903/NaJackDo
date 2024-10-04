f7cadad091580eba417d83562f30397571591e4a
이인준 <dldlswns890@naver.com>
2024-10-04 17:36:49 +0900

    [FIX] baseprofile
    Former-commit-id: ac85d6231e721fb2bc99627883b5d814705cca2c

--- client/src/page/library/components/SellerInfo.tsx
+++ client/src/page/library/components/SellerInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9963fc972c34504f6c753a7f923fade36976a630ffa6353cb32e8c71f7b1d415
-size 1735
+oid sha256:7a6157a7862ab82e682c2c6338abb981858aad9788d40830a44ca072a9b5041d
+size 1724
