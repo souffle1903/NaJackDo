68c30f594d33b90d05251807a4a5020593191d1b
서민수 <minsu9812@naver.com>
2024-10-04 17:35:36 +0900

    [FEAT] 베스트셀러 연결완료결
    Former-commit-id: f6bdc085d2d90d4b21956506501b8d8864af9b94

--- client/src/api/bookApi.ts
+++ client/src/api/bookApi.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f2771703d2c4cef30e74d9b3d30051b599547447b25c0b4809df68f11650e1d9
-size 4988
+oid sha256:81ff1f9ec3a8f5a5df4929a0bb488c2f4c489641e157879f5bf154edc9478fe3
+size 5448
--- client/src/atoms/Book.type.ts
+++ client/src/atoms/Book.type.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f70491f8933bd1eeb94caa4267aae7418d9b9c3e327af1ca6a15ca5c7f4405b
-size 936
+oid sha256:c82e5bc28443b7ce4ca06557487eac6ee55b6265308ff13b297259182357fdd3
+size 1223
