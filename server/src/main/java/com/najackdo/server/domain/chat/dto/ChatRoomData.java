9ac07ae847c319e8f7a3ba8a7bcc9506d306e713
SeongMin Hwang <qq221qq@naver.com>
2024-10-04 10:33:33 +0900

    [FIX] 채팅
    Former-commit-id: 17ad3033602d9bd1c7e7bd6c2785f8f635c597cf

--- server/src/main/java/com/najackdo/server/domain/chat/dto/ChatRoomData.java
+++ server/src/main/java/com/najackdo/server/domain/chat/dto/ChatRoomData.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7df7e62ccc4c68d8841e72aaf2d853b993d473eb2296908bfdd623e141dcfe29
-size 2813
+oid sha256:99dd9a85e088a3a98242b27180f07c18f352fb93c79b00b6153d15782425701d
+size 2809
