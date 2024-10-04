2c2163ae38c9c6cbcaa2ab430a5d2684fdb0fce3
김도영 <rlaehdud1002@naver.com>
2024-10-05 00:53:44 +0900

    [FIX] 모달 추가, 아이콘 사이즈 정리 등등
    Former-commit-id: 06a6d919e45b218f770dd88e74cdb549f313304f

--- client/src/page/search/page/SearchPage.tsx
+++ client/src/page/search/page/SearchPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:291244800205a15aedc5bd1b7500bb7cf73a356d6e22427a337eca0f7983cf87
-size 2107
+oid sha256:de3c6792f93b9238e92d6ca9a99179ef4c02b225f5bf0daa39e9862cc4420405
+size 2083
