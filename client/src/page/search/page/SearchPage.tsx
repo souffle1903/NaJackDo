454854d021eb72a8bfc6d4faf28ca56f699690c0
이인준 <dldlswns890@naver.com>
2024-10-04 11:31:03 +0900

    [FIX] myleaf -> kapay / 검색창에서 뒤로 가기
    Former-commit-id: 69f7d08c049b1ea853b83c87f0520deb59171e24

--- client/src/page/search/page/SearchPage.tsx
+++ client/src/page/search/page/SearchPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66c4c643ca084b5368a39443f89d65504b0c7d8c931d50044e96acef2b2fb4df
-size 3764
+oid sha256:e7f49c58be37b67e2a24b185135ae4787d85455856cc640cc8f2a3beb4306332
+size 3866
