b827f4e838ab368100a39ce4df6eb4d97cfdd875
김도영 <rlaehdud1002@naver.com>
2024-10-04 23:42:57 +0900

    [FEAT] 좋아요 alert 모달, scrollbar npm
    Former-commit-id: 003910cdbe527c9cd7d4ccd439bc97f966fa0ba1

--- client/src/page/library/components/DetailRecommendBook.tsx
+++ client/src/page/library/components/DetailRecommendBook.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bfeef30336163bf75eefa24da9ce1a764777c8dbd1be63d0f996fee3dbf32ed9
-size 1533
+oid sha256:6f25af21e825b13848f77f3a7f8742e96a2b3b76728936955eafdb4d41d46079
+size 1485
