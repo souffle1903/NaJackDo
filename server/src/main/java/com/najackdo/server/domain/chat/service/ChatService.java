87a7e058ec2e02753decfa5de853958b6cbba173
SeongMin Hwang <qq221qq@naver.com>
2024-10-04 15:54:34 +0900

    [FEAT] 채팅방 생성 시 카트 isDelete처리 수정
    Former-commit-id: 369efbd54ccd407f5da3dab79e47e65e81a0e16f

--- server/src/main/java/com/najackdo/server/domain/chat/service/ChatService.java
+++ server/src/main/java/com/najackdo/server/domain/chat/service/ChatService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:353d09e3f036f6f5b1d12d6dbf801f61edcf65f6bca1246447893377dd363320
-size 3216
+oid sha256:bcd291b1261ce299eec74110af20a70ad5a9d8a42a95c277cc6623a1149ada81
+size 3217
