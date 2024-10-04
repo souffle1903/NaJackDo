252de01a85c31c1dd47ff9b1fb426bee0bcb64e1
김도영 <rlaehdud1002@naver.com>
2024-10-04 16:37:46 +0900

    [FIX] 여러가지 수정
    Former-commit-id: 169272885174a7c089b5f62eb4db589aad0b23b9

--- client/src/page/chatting/components/CartModal.tsx
+++ client/src/page/chatting/components/CartModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a4ebeb4c69383e1ad15abd8ddeb9246179c1c6d9e4eb53105f68d7c276db909
-size 2529
+oid sha256:75234b07c3f4968ccc4bcf841a9792de0eab4a72c6a3cda05f0eb8062c2b82f7
+size 2548
--- client/src/page/library/components/HistoryList.tsx
+++ client/src/page/library/components/HistoryList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aba973c0147702a33520683af167879fab617d8d14655eaa9dbf5d4a43fd904d
-size 2311
+oid sha256:a0c13872a8672dbebda4dd6c71c2ae31f3eb6237b155a12864a66548ddbc9186
+size 2376
