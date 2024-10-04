65070def7a785e560706e89c1dc6a95c4eb5132a
lij0825 <dldlswns890@gmail.com>
2024-10-05 00:52:06 +0900

    [FEAT] 후기 남기기 버튼 로직 추가, rentalId 리페치 로직 추가
    Former-commit-id: 282fcbcb75dc8808c140fbdb0db333ed46fde989

--- client/src/atoms/Chat.type.ts
+++ client/src/atoms/Chat.type.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:717dcd87ac7932910b6f2dce1200c8e7737d230640698f5e757e3d69e6a1cc39
-size 733
+oid sha256:2212e08da58476ad20b28da5907d66f44882ed8381a287c2d00d6706097f0fbd
+size 808
