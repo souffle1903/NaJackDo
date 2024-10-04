9f99691b8cd72fd9a0cb73bc819cc6f8a8197c76
SeongMin Hwang <qq221qq@naver.com>
2024-10-04 16:49:37 +0900

    [FEAT] 내 지역 베스트 셀러
    Former-commit-id: 84ed61de72171e67b6441bc0517d057d3b4b6ec5

--- server/src/main/java/com/najackdo/server/domain/rental/controller/RentalController.java
+++ server/src/main/java/com/najackdo/server/domain/rental/controller/RentalController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ee13ec530fc1aea25a898281e2995c68274471b6427771fac2cb612a52adfaa
-size 3054
+oid sha256:1bb4be3c91e492bb525cec3e87b599c8293b3d8d051c0e4356938983e36416b3
+size 3418
