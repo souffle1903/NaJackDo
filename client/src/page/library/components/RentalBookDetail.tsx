b0f60b73ae865dadf087d061cf592266f13c5403
Chung <souffle1903@gmail.com>
2024-10-04 16:52:42 +0900

    [FIX] 상세 페이지 오너 프로필 및 책 제목 추가
    Former-commit-id: 734c7d767edb13ab78dee6301584d76ae70ffd86

--- client/src/page/library/components/RentalBookDetail.tsx
+++ client/src/page/library/components/RentalBookDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dab3aca30078839d8b9749f62a92f9bcdbfdbfe26db33e3f56207eb8da9c45a0
+oid sha256:32b8e85c2e00012dc1da9f66690a030fc9d19869951713d1e65b2900f54c1173
 size 1303
--- client/src/page/library/components/SellerInfo.tsx
+++ client/src/page/library/components/SellerInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:31a5925ea68eb1f27283a520785aaf1dc311ff647cb8eda2ff04652df03e310a
-size 1590
+oid sha256:9963fc972c34504f6c753a7f923fade36976a630ffa6353cb32e8c71f7b1d415
+size 1735
