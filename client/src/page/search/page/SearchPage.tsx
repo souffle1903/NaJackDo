552aec6b5e9da8d8e2aae908cd389c0bcda2bf59
김도영 <rlaehdud1002@naver.com>
2024-10-04 12:02:01 +0900

    [FEAT] 로그아웃 세션 초기화, 검색 클릭 시 상세페이지 이동
    Former-commit-id: c31f366d88885eba7f722d10b2942a6db21fac31

--- client/src/page/search/page/SearchPage.tsx
+++ client/src/page/search/page/SearchPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5bb61d6ed2a472d57241b4202b1a6d1225f9f5a9dd0368963b10f8d9c0e5bf47
-size 3617
+oid sha256:fdd122bdea392971fbfef13028689f58fa325dfebfed1088d2f0279213894ff8
+size 2192
