64bf56163f6f2527033433abcdfc0bce53718108
SeongMin Hwang <qq221qq@naver.com>
2024-10-04 13:42:09 +0900

    [FIX] 리뷰 두쪽다 작성 시 채팅방 삭제
    Former-commit-id: b07d0358412e63e0b380c670b90ee55eb6b0a1b6

--- server/src/main/java/com/najackdo/server/domain/rental/repository/RentalRepository.java
+++ server/src/main/java/com/najackdo/server/domain/rental/repository/RentalRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a7b647c7afb0123b8e58ea71af265ba2f517e3438833f6bc8f566803de3f0145
-size 1607
+oid sha256:053f47df9b819f76f99f86b34a5345f485955e3df0dfbcf4be81fa5eaa153b0d
+size 1852
