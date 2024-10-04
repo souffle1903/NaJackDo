4f7ccd166c7129ecf08991b2860a418922c24898
lij0825 <dldlswns890@gmail.com>
2024-10-05 00:52:06 +0900

    [FEAT] 후기 남기기 버튼 로직 추가, rentalId 리페치 로직 추가
    Former-commit-id: 17f42d89619fd2130b617f429e0238acd07f872a

--- client/src/page/chatting/components/ChatBookInfo.tsx
+++ client/src/page/chatting/components/ChatBookInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c34bd33ae32d636e52f9406c7606af52cb09815c56b7527fb3a40adf908cb6e
-size 6716
+oid sha256:58438f4024f1748f15c3df210f5a94b47f02f02abc73e550d4babe66c5fffc6d
+size 7809
