b561f973eb59a0773bc7103478d46de452dadbe3
SeongMin Hwang <qq221qq@naver.com>
2024-10-04 21:34:29 +0900

    [FIX]
    Former-commit-id: a6a783abfeacce5960ced8e05ee96d9cb1bd3c72

--- server/src/main/resources/recommendation/app/dao.py
+++ server/src/main/resources/recommendation/app/dao.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5371b3afd6b3b9f99ece84775c3f0cb258601da80ad3f5f2413d902c5ff65135
-size 3496
+oid sha256:c4456be5ce0dd114713d28840a55df502bb7c06c35b26b7c00aec6c8dc35decb
+size 3497
