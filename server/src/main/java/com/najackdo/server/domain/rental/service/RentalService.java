42e3d43f9351dd9c23d8f9701f45b508a8063667
SeongMin Hwang <qq221qq@naver.com>
2024-10-04 09:57:29 +0900

    [FEAT] 베스트 셀러
    Former-commit-id: 17274b28bb6e21bde690a355534b2db9db338065

--- server/src/main/java/com/najackdo/server/domain/rental/service/RentalService.java
+++ server/src/main/java/com/najackdo/server/domain/rental/service/RentalService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e234568b2ad8d2e52e11bfdc685d37a4e7ad6d4d6118b7378ad2fa7d43216c81
-size 5574
+oid sha256:2845e6327fb87197c79e988bb918b7087438649e171acf8059244eab6283c14e
+size 5819
