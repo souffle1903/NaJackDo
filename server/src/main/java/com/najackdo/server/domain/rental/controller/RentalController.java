94e5f02c501651e92bfbc314a9a922b823436f95
SeongMin Hwang <qq221qq@naver.com>
2024-10-05 18:31:23 +0900

    d
    Former-commit-id: ea0b1bc57fb6ffeddca1277d00d04ada6fb66040

--- server/src/main/java/com/najackdo/server/domain/rental/controller/RentalController.java
+++ server/src/main/java/com/najackdo/server/domain/rental/controller/RentalController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dca5a43ac1f04f9ef547aeaa9b18397dbca061e51ba766c6f8d28431b6b2a738
-size 3051
+oid sha256:127c133e5657942bb08d19f30b9bd461789d6f7a89fd0f5a7cdf09cb70e2a28c
+size 2995
