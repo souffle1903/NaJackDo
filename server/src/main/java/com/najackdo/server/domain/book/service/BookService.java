c2e47297e44bcb9d583788a42bca65ef44a66e3a
SeongMin Hwang <qq221qq@naver.com>
2024-10-05 14:40:29 +0900

    [FIX] 도서 검색 페이징 처리
    Former-commit-id: 06b2a1a01886c406556404bb115c4147d89b8927

--- server/src/main/java/com/najackdo/server/domain/book/service/BookService.java
+++ server/src/main/java/com/najackdo/server/domain/book/service/BookService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a1c3797a3459aa38238db9c7485bebde0d5384b6eb8a7254db50a31c5cc76942
-size 7464
+oid sha256:c704e703e279abccf1bae884305c75895bab4433163f7b784151f6232a81212e
+size 7463
