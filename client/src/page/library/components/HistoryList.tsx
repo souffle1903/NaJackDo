2409c2b1256a11ca4e88cd837621caa32947517c
김도영 <rlaehdud1002@naver.com>
2024-10-05 15:37:17 +0900

    [FIX] ui 수정
    Former-commit-id: 49a5575b640da7a841451c5119b218e3734bceed

--- client/src/page/library/components/HistoryList.tsx
+++ client/src/page/library/components/HistoryList.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:87aafbcbd93b32ffabde8f4aa6435fd516574a01e82e3ea86e0d024bc531b9ef
-size 2517
+oid sha256:5dbdeef36a234a692817dea2696f7a0ec815af25e44cb22d3bcbf7428b88bf53
+size 2654
