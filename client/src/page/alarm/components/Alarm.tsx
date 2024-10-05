51d8710ad1179f85bcedd70e6f65e352b5d6d9bd
김도영 <rlaehdud1002@naver.com>
2024-10-05 15:37:17 +0900

    [FIX] ui 수정
    Former-commit-id: a9df6114471883e8d72132c1ecbce9c3c0bf653a

--- client/src/page/alarm/components/Alarm.tsx
+++ client/src/page/alarm/components/Alarm.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8028c630740877003404b7529ccc35eacaec0cf645eae6416af8255f4252e91
-size 1542
+oid sha256:beee43be6ee5a9ae15d6c296b81a73cf39c6c1678c8692866a9b496775dff971
+size 1552
