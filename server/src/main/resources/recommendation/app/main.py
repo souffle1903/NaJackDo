eb785666303ac5909b26715fd0e0eb3d19f65c44
ParkBeomSu <beomsu4639@gmail.com>
2024-10-06 06:05:38 +0900

    [FEAT] complete api
    Former-commit-id: 9c7a366a1c533d8ec6dc66e797bfcbdc07a5f088

--- server/src/main/resources/recommendation/app/main.py
+++ server/src/main/resources/recommendation/app/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3d385b37bc07a218f0cde245654b36eb75491922cd5254259279e073aa02700
-size 9093
+oid sha256:9ee5d08ebac2d754b39b880edaeda150c390cca81637dc8f21f57536098a1913
+size 12445
--- server/src/main/resources/recommendation/app/reco_sys.py
+++ server/src/main/resources/recommendation/app/reco_sys.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9572faedc75f29e396b9794cd826b3bba798b8888d99c0336e3b81bff128a6ae
-size 4814
+oid sha256:a743f9cbcc4b6655f106cf401b7ea55273cde3fb6f747e8c5821d74512588e88
+size 4824
