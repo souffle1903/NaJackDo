0eb7262fcdea4deee563d17aa3876c4f52bca8dc
chung <souffle1903@gmail.com>
2024-10-05 18:43:18 +0900

    [FIX] 상세페이지 제외하고 헤더 보이기
    Former-commit-id: 67606ba2b68a2459e3c8e8e8316480c4181599af

--- client/src/page/chatting/components/ChattingHeader.tsx
+++ client/src/page/chatting/components/ChattingHeader.tsx
@@ -5,9 +5,6 @@ const ChattingHeader = () => {
   return (
     <div className="flex flex-row justify-between items-center py-4">
       <span className="font-bold text-2xl">채팅</span>
-      <Link to="/alarm">
-        <IoNotificationsOutline className="text-3xl text-[#545454]" />
-      </Link>
     </div>
   );
 };
