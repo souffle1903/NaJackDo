641f90cc5be5c74642f21a64d70da61cad9180f1
김도영 <rlaehdud1002@naver.com>
2024-10-05 15:37:17 +0900

    [FIX] ui 수정
    Former-commit-id: dfe7790ea0a6d78b94da442577af16392c0bccf1

--- client/src/page/search/page/SearchPage.tsx
+++ client/src/page/search/page/SearchPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de3c6792f93b9238e92d6ca9a99179ef4c02b225f5bf0daa39e9862cc4420405
-size 2083
+oid sha256:2022cb89ffa751cfb738c0229fa21fe37074a8ef947945193981ea517e5e251c
+size 2365
