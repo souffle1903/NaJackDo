b81b895f2388a902e7eb5b887c92194b44125e43
SeongMin Hwang <qq221qq@naver.com>
2024-10-05 18:43:09 +0900

    [FIX] 렌탈 기록 몽고 디비 기록 수정
    Former-commit-id: fabb0d4014a807473da6720198d0da55e3244627

--- server/src/main/java/com/najackdo/server/domain/rental/service/RentalService.java
+++ server/src/main/java/com/najackdo/server/domain/rental/service/RentalService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc128bae49f40b4cedf03c0056e63ce5da78b26f54181b1e044f53d331b3ee3e
-size 7150
+oid sha256:a627239b63d838fd4dfe34b650b48c7126723bb49ca25cc6edb978fb12790dda
+size 6914
