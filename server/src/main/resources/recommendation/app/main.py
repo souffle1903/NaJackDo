9f4acca89132924ff76ea67c5fc264a5f3c55c28
SeongMin Hwang <qq221qq@naver.com>
2024-10-06 20:49:39 +0900

    [FIX] 도커 컴포즈 파일
    Former-commit-id: b671b3e5d551501ef96b908393b7b3b8280afb0e

--- server/src/main/resources/recommendation/app/main.py
+++ server/src/main/resources/recommendation/app/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3e3c144ee96c7f7b30e3fb80b7748341bd622591c76a3e072df5107f6b592a70
-size 12461
+oid sha256:c2c1b6e30da57387a8881d40e377ca1a0829aebf572374bb7f1a2542f646e8a3
+size 12478
