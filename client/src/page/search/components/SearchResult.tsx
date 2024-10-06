7258b50fe6e07a0131bed8b716af6386200eacbc
lij0825 <dldlswns890@gmail.com>
2024-10-06 15:44:57 +0900

    [FIX] 검색 스피너 추가
    Former-commit-id: 7afcbbdb3cf40ac38ba865625be97dc3a83c5b7a

--- client/src/page/search/components/SearchResult.tsx
+++ client/src/page/search/components/SearchResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6d28ef280a742e32ccaf566bbd4ec02ff432f08899d6641ca6f79c136e517e0a
-size 2382
+oid sha256:58a3b905196522f1afc8b0acc47c17c2af40f027036af7693f7ffff472d34f9d
+size 2496
