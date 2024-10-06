66fafc76caed905fd9e5e7115994b7799be086aa
chung <souffle1903@gmail.com>
2024-10-06 18:15:52 +0900

    [FIX] 지역 설정 및 상세페이지 모달
    Former-commit-id: d434b0fc0509012484b5082dde7d6514c126b43e

--- client/src/page/location/components/RangeSetting.tsx
+++ client/src/page/location/components/RangeSetting.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0afbec0d7661bcdfc461865081c98551eada2b0dcc248ca693e0f4fdc1458201
-size 4925
+oid sha256:1527b4813923f26c506c099b94fa90faa219940c41def41fdfefc67d68b85450
+size 5182
