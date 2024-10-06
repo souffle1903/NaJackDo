c200164e0712850695cbed90c452de9d8c02198c
김도영 <rlaehdud1002@naver.com>
2024-10-07 00:37:29 +0900

    [FIX] UI 수정
    Former-commit-id: 2937ab0a42cb1acd5c3d5d36102b3fce958c0be1

--- client/src/page/chatting/components/ChattingHeader.tsx
+++
@@ -1,9 +0,0 @@
-const ChattingHeader = () => {
-  return (
-    <div className="flex flex-row justify-between items-center pt-4">
-      <span className="font-bold text-2xl">채팅</span>
-    </div>
-  );
-};
-
-export default ChattingHeader;
