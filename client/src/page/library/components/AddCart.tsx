ad268e765423ad5dd1584ddab5dedae579a98ee3
Seo Minsu <minsu9812@naver.com>
2024-10-06 17:29:18 +0900

    [FEAT] 책 상세페이지 수정
    Former-commit-id: 0a3af50701fc3bc95e2f1e555ee5f72459a9530e

--- client/src/page/library/components/AddCart.tsx
+++ client/src/page/library/components/AddCart.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:74b320b403eafc7cfe8f4eb564e0e5e86d2b7c773db45886db0c5e4abcb6fdf2
-size 945
+oid sha256:cc39e9efadeb3ff692512aea088cb533d675b4baada94be3b85fd1d4f817ad98
+size 952
--- client/src/page/library/components/RentalBookDetail.tsx
+++ client/src/page/library/components/RentalBookDetail.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:affb088d1864341da5df0906777d7654c77ba48f7941eb73b907c36cdc770f8e
-size 973
+oid sha256:02a6de3e42c0f2b95fb2564c63f48eea2cc241c8eb18d569e828a6a65f57e0b3
+size 1678
