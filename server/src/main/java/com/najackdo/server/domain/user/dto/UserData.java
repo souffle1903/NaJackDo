c69e5862125772c95a994c99b502ab5703af2cae
SeongMin Hwang <qq221qq@naver.com>
2024-10-06 18:36:05 +0900

    f
    Former-commit-id: 78c02323c44c6699ba7e25e74289ffac348a4df2

--- server/src/main/java/com/najackdo/server/domain/user/dto/UserData.java
+++ server/src/main/java/com/najackdo/server/domain/user/dto/UserData.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37e363b7c2baafa19ffbf54cbf6ff7a2dcabc10842e57865b3e96dea14cacac5
-size 4114
+oid sha256:c38799c9ce5c312390e8453da574592d8d2711552cd12a33b8e8449b51a2290e
+size 3754
