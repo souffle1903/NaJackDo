71e2dd50956ce22128a0978ae777f0b87b5a62b3
Seo Minsu <minsu9812@naver.com>
2024-10-06 23:45:25 +0900

    [fix] 타인프로필 에러 수정
    Former-commit-id: 88bc6401ef9ba23c306bd6f6e66e9e3ecd0080d3

--- client/src/components/common/BaseProfile.tsx
+++ client/src/components/common/BaseProfile.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b21c0bb12f3983b77649b797203ce37734b44582fd3d4e450d1d401cc7718ba
-size 602
+oid sha256:a56eda7f2ebc192125b3a7e359356fefbf43c5dbc08c35e044bc54ea84848f51
+size 868
--- client/src/page/library/components/SellerInfo.tsx
+++ client/src/page/library/components/SellerInfo.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce2c83efb0f244cf4bfe93e1ec7c37e2cee744e852b85adb7d59120a2751ca66
+oid sha256:3e43b69825a8121b842f354d77bbec22a913b37d1cd57e7fb27a7ab974580ba7
 size 1772
