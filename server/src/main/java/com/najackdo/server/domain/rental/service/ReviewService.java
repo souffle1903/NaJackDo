2005e98d136f459a466ce97f45f2d80cd866b62e
SeongMin Hwang <qq221qq@naver.com>
2024-10-06 18:36:05 +0900

    f
    Former-commit-id: 9e4f1351a14aad5a4abfcccdb9f9127441aed024

--- server/src/main/java/com/najackdo/server/domain/rental/service/ReviewService.java
+++ server/src/main/java/com/najackdo/server/domain/rental/service/ReviewService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:158bdc7a7bc831113fbe28a96585c35ab260acda94f4605c624c3adab8768144
-size 3824
+oid sha256:c23f2166d1cce02c832918c6067777f03ea95850133202cb786feda3638b4885
+size 3822
