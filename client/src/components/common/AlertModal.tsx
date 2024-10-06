d62f2efd588fd338e38a58ef6d91a4608b24b0b6
Seo Minsu <minsu9812@naver.com>
2024-10-06 17:29:18 +0900

    [FEAT] 책 상세페이지 수정
    Former-commit-id: 996ea1c9671b0da2bc2c07362399769e2af3d37c

--- client/src/components/common/AlertModal.tsx
+++ client/src/components/common/AlertModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:43d4bc32d05afb1061f4ca791cafe5e2ce3302ee56182d413c7e91c2ed5ffdaa
-size 1028
+oid sha256:debca53a7b5acaad2941a130a2329225f3e5243df72095548c70c1eb04aaf5b2
+size 1035
