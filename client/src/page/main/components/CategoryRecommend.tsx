664bf1db3f63b5ecfb3edacc05d68bf2f24c8ec5
SeongMin Hwang <qq221qq@naver.com>
2024-10-06 16:03:26 +0900

    [FIX] 장르 종류 수정
    Former-commit-id: 7e33aaa3a0803c4b0b624b7b43903e3089341a61

--- client/src/page/main/components/CategoryRecommend.tsx
+++ client/src/page/main/components/CategoryRecommend.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:47f71d527e25b0227f83bdb71d9c31afd4ab845c3c1d6a5e8e3a56f706be7e56
-size 1690
+oid sha256:badfefee29d6aff11e431c08b16e4b882d30fefe425bfb9e3850c57ec1c25de2
+size 1729
