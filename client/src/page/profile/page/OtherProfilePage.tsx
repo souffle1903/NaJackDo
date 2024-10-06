901ec8fa5838678409ca18270c119d670d78b222
김도영 <rlaehdud1002@naver.com>
2024-10-06 15:26:32 +0900

    [FIX]  리뷰 카운트 수정
    Former-commit-id: 944f8d24447df4b3e5b3615f8de95a6a050b096c

--- client/src/page/profile/page/OtherProfilePage.tsx
+++ client/src/page/profile/page/OtherProfilePage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72d5a148f4c97ff7339fddff286e89793b398f4ea25d65f2940ad79f38ba17b3
-size 2924
+oid sha256:9dced4497526babfe8f29565e0b1878e42aab99b35a35daa957b235de0944b1c
+size 3116
