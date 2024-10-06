381fb481e8315f4ce4fe89e422fdc1b44e38fd77
SeongMin Hwang <qq221qq@naver.com>
2024-10-06 15:12:32 +0900

    [FIX] 이것저것
    Former-commit-id: 7c977d2d679cb847ef5c64122992459a4a4ab4c5

--- server/src/main/java/com/najackdo/server/domain/rental/repository/RentalRepository.java
+++ server/src/main/java/com/najackdo/server/domain/rental/repository/RentalRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7aba6ea71d3aa80e5afee2cc712009333ffc6f9836ced4da7a5bede748a8752a
-size 2394
+oid sha256:094b14e805f07dc0c43a733d90a7054ce7e5228de89c40c0d94b0d1fd058ec74
+size 2533
