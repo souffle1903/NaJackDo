9049fa8ce71747f55d99093e808f725cb6b25dec
김도영 <rlaehdud1002@naver.com>
2024-10-06 21:32:32 +0900

    [FIX] 대여 가격 수정, 스크롤 막기 등 수정
    Former-commit-id: 6c6dc842a6fcb1d987cf0a9023a85047f2e00511

--- client/src/page/library/page/RentalBookDetailPage.tsx
+++ client/src/page/library/page/RentalBookDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab16157c68612f26e5f2df60d7266796a34a77961a91d55d06cfb15725011a2f
-size 1168
+oid sha256:c0ae0c448b7847cc349787d95a8a8956c3fff859d5e09acc832dab22bff40c25
+size 1162
