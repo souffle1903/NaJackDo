00e9dc07ce66673e6f7875f07cadc54f3cede666
SeongMin Hwang <qq221qq@naver.com>
2024-10-07 12:01:21 +0900

    [FIX]
    Former-commit-id: 17e789feb747f2ca97d46f2623b9a95eb89cc302

--- server/src/main/java/com/najackdo/server/domain/rental/service/RentalService.java
+++ server/src/main/java/com/najackdo/server/domain/rental/service/RentalService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9fee2568320d7504d9f299a20e656c7e1f41f002b638bc030e1e556e94dbb67c
-size 7481
+oid sha256:a70f000f0ddda56e088439760ef012ba55ab7afc1d8a5e0c9b7e58e8df93089a
+size 7486
