629d6fa99026686dc2ba57abede2bda34c43e28a
Chung <souffle1903@gmail.com>
2024-10-07 16:58:49 +0900

    [FEAT] 신뢰 나무 점수안내 모달
    Former-commit-id: 2b45136056434b83e3631e5fb81a706c0deada15

--- client/src/page/profile/components/UserProfile.tsx
+++ client/src/page/profile/components/UserProfile.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c90bc5c727843430d39c18aa65e2dac0c31c89572f363a2297a35cce75414567
-size 1061
+oid sha256:919859047ef649e2da08b4108dd6bf127e0aaa1caae2df9f153fe4cef11cf1b0
+size 1143
