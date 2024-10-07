7eecf4070a97d38426f688fb27e3819a12607d77
Chung <souffle1903@gmail.com>
2024-10-07 10:22:17 +0900

    [FIX] 채팅방 키보드 화면 밀림 수정
    Former-commit-id: 7d773a9f165c326b6ee8b122a29d8670180d5ded

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8ff833ee261949011d23dead739021799e45617d27bbafb183c6c361dfd58c4
-size 3763
+oid sha256:5499b4aee9b86297183bf3466cf03ea6afdf277dd91d875d935a7454c1817139
+size 4442
