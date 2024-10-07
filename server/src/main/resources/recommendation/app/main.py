67d8e3524216ac0d3e98bff27ea8bc96fbd1f401
ParkBeomSu <beomsu4639@gmail.com>
2024-10-07 15:35:58 +0900

    [FIX] 버그 수정
    Former-commit-id: 4e4ad37ea52e8e73d97b9e7a2e170e48aa4d2d6c

--- server/src/main/resources/recommendation/app/main.py
+++ server/src/main/resources/recommendation/app/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:276d020ddaa5f423ce3840d4e5b0a2f23f223ef78b9a6af3480dd29c9896a9de
-size 12481
+oid sha256:08c3469e1b239169ebd7d6e6fcbf8e889d1a0da991b187f9a8ef21bff52a2ff1
+size 12465
