0fd59d95423d0a614666261b665a15a29b9c2654
김도영 <rlaehdud1002@naver.com>
2024-10-07 09:30:09 +0900

    [HOTFIX] 알림 수정 get -> post
    Former-commit-id: 13a598504a9a4852870b5c1d498688e1471f07f1

--- client/src/page/alarm/page/AlarmPage.tsx
+++ client/src/page/alarm/page/AlarmPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:35ede32f214e8304b05dfd2ef7c64f9756ee7c3143a787cb063b5c68a1c45d98
-size 2705
+oid sha256:906c3e61dd30f8798eb8cd5459d98ba9182b1ee65f5530f549455efb7b16250e
+size 2638
