0075ae52dc6a33f352f33698fabf7c28d5ef3473
김도영 <rlaehdud1002@naver.com>
2024-10-07 10:34:23 +0900

    [FIX] 추천 로직 수정, 헤더 수정
    Former-commit-id: 9bb1006d8bc9c32c662a56d6e1e0ea6696590386

--- client/src/page/library/components/DetailRecommendBook.tsx
+++ client/src/page/library/components/DetailRecommendBook.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc218b6c8ba764d498f3fec2e522a1964e69b9e9acfd6141fed584862b950bc6
-size 1314
+oid sha256:eab5925461bda65defc7a1e8055de78473169cf9a3cb2d6e350c442ff801721b
+size 1449
--- client/src/page/library/page/BookDetailPage.tsx
+++ client/src/page/library/page/BookDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afa3f3a978ae9a50a58859e1549c94d824d99dd5c3e31805571c0d40104efb58
-size 964
+oid sha256:bdba840e04e155355364cd98c91e81353ac491f81803f4d3431fe22576e5da54
+size 1246
