3af17bed27560de29341903fd01fb9b64d7f01e6
SeongMin Hwang <qq221qq@naver.com>
2024-10-07 17:30:09 +0900

    []
    Former-commit-id: 04adebf8354f915c08b84288b35055edd26bdd71

--- server/src/main/java/com/najackdo/server/domain/book/controller/UserBookController.java
+++ server/src/main/java/com/najackdo/server/domain/book/controller/UserBookController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:30e2da78184334cc14317d601b8045624e84f8ccdac755cd4cc2f6562f570a9e
-size 2558
+oid sha256:403cf22fd3209933a54cfe396bcf2c382978ae4309e9f65b1372a66a94058a4e
+size 2588
