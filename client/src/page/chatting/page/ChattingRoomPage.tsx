26a46ba455e1dbbb66400989f0774b0d7497e8cd
Chung <souffle1903@gmail.com>
2024-10-07 14:59:06 +0900

    [FIX] 채팅방 키보드 마지막
    Former-commit-id: 9b1abfd4751afa5c4c3058ff2a026e60849e1cc7

--- client/src/page/chatting/page/ChattingRoomPage.tsx
+++ client/src/page/chatting/page/ChattingRoomPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0fb8ee43df1383e42fb45dbaecd14e17aea821a05f46f189c4e566c700ce647f
-size 3781
+oid sha256:246bbb686a08d3ed1148d81a9a82c7aae6bf21accab78f8bf2a085e714919138
+size 4092
