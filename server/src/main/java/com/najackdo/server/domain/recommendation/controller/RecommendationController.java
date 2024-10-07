830af5e88023b52a01f933cbe28b62b619f49a66
SeongMin Hwang <qq221qq@naver.com>
2024-10-07 10:01:00 +0900

    미안^^
    Former-commit-id: 5dab97b3d8a908d2b491797ca70381df5b04549e

--- server/src/main/java/com/najackdo/server/domain/recommendation/controller/RecommendationController.java
+++ server/src/main/java/com/najackdo/server/domain/recommendation/controller/RecommendationController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e68e6e6fb73f3984b6241476f54f2cf4af20dc100710fa35bc784ab513e55aa
-size 2072
+oid sha256:f32423ef27ce9f9bcf2fcf54f1738090780dffa557dd6cd74a5c150b8449c21d
+size 2063
