a50420a1049f77c15a7cb7a3f9d687336a31e0b4
Chung <souffle1903@gmail.com>
2024-10-07 15:06:14 +0900

    [FIX] 채팅방 포기
    Former-commit-id: 814c8e43007c412608ecb83eada9daac64bb693a

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:583711c79fa54670cd6a8c5d77b6cffabcc09f8eb61b0f937ca7b6cd356443e4
-size 4642
+oid sha256:ef76f93b0832d4a13df0907e66a9a85f9828410591d3e89e44d7ce66a25892c7
+size 3790
--- client/src/page/chatting/page/ChattingRoomPage.tsx
+++ client/src/page/chatting/page/ChattingRoomPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:246bbb686a08d3ed1148d81a9a82c7aae6bf21accab78f8bf2a085e714919138
-size 4092
+oid sha256:0eb263e9678801ef9b4f4252b7aa63453fc01b778b295f3688a51b4e903fbae1
+size 3397
