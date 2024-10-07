ad8065cced59a1ec71c3975d59eafb12faaa6bb4
ParkBeomSu <beomsu4639@gmail.com>
2024-10-07 12:07:50 +0900

    [FIX] 예외 수정 및 속도 더 줄임
    Former-commit-id: dca2732ec981ba07bfe24bf9e66bea43beca24d2

--- server/src/main/resources/recommendation/app/main.py
+++ server/src/main/resources/recommendation/app/main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d8998c39734acce657b4fddf2ef3f73f3c52af7ed3789c19fea933a85a28337b
-size 12506
+oid sha256:276d020ddaa5f423ce3840d4e5b0a2f23f223ef78b9a6af3480dd29c9896a9de
+size 12481
