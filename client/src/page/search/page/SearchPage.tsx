2635aca576cfe6be4351f7bb0f22d29e58e6d5a7
김도영 <rlaehdud1002@naver.com>
2024-10-07 12:06:57 +0900

    [FIX] 로딩 수정, 빈 배열 예외처리, 장바구니 에러 처리
    Former-commit-id: 9b7994b0e5309552f6d8fe2d68bd1349e725af79

--- client/src/page/search/page/SearchPage.tsx
+++ client/src/page/search/page/SearchPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57f3cb8c53c27ba5e6e46df6592d6210dda8922b8cdae3c7e5bcfbd2ee291a22
-size 2395
+oid sha256:87ca93f6b4af91e4036c8eb99a88ef8e4be5dce253ceb102aa270ef58b5d8546
+size 2429
