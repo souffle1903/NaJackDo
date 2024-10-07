fbe7b6a8813d6de4572453a9faf3124289f312c7
SeongMin Hwang <qq221qq@naver.com>
2024-10-07 16:06:42 +0900

    [FIX] 추천 개수 수정
    Former-commit-id: b0ebd4ace8495d428b60e246b1b1ff44dfeef0f7

--- server/src/main/resources/recommendation/app/reco_sys.py
+++ server/src/main/resources/recommendation/app/reco_sys.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:472fcf3ecb1cc3be3043c5b42d02146cc3730a3dddac4c6be6f0f3b0672af7a2
-size 5618
+oid sha256:4acdbda219849b0fb60a23a4a5a01a0c0eea25a270cea9803f12565d5435c512
+size 5619
