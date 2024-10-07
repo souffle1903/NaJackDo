b2676842c1543e0284b1fe19249b33f303abe80d
lij0825 <dldlswns890@gmail.com>
2024-10-07 22:31:17 +0900

    [FEAT] bookids로 추가하는 api
    Former-commit-id: 2523c185a06364d71fcbeccd9a593096efb9b4fb

--- server/src/main/java/com/najackdo/server/domain/book/service/UserBooksService.java
+++ server/src/main/java/com/najackdo/server/domain/book/service/UserBooksService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d25f06233e00dfd22c98f074a3b88dd3e7a92ca6482f619214869a3b3385c209
-size 9144
+oid sha256:654575cea7646bf5d56806ea22389dec0b8e3141f74e5570738894dc8de8ab62
+size 9892
