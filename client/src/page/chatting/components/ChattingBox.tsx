c39d02573365a8ea3fbc12249f32c51894e5de6d
Chung <souffle1903@gmail.com>
2024-10-07 12:15:10 +0900

    [FIX] footer 내려가
    Former-commit-id: fa46a7f6cadb73013d914e3dcb0b08b1237aca90

--- client/src/page/chatting/components/ChattingBox.tsx
+++ client/src/page/chatting/components/ChattingBox.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:06e197a5a131d2977a81c7a2ff613b3d2f3a7c1377120fc0ec0e7ae093d09660
-size 5165
+oid sha256:213213113e6fde2b0435844644c5512e6308e268e85e3f3fce7dc01daf87bdb9
+size 4564
