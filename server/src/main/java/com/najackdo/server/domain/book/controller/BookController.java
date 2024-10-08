af750bd94b306016843744f7944380f2daa782b0
SeongMin Hwang <qq221qq@naver.com>
2024-10-08 11:14:03 +0900

    [FIX] 책장 등록 시 이미 있는책 구분해서 반환
    Former-commit-id: 09eb280b07a91d062de87ba5ecddb95cd5a9dc92

--- server/src/main/java/com/najackdo/server/domain/book/controller/BookController.java
+++ server/src/main/java/com/najackdo/server/domain/book/controller/BookController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ffd484e46696d7b5264224a5c1e1fc13fb160f1d89b75a33a1bf3b1d85af71b
-size 6598
+oid sha256:13e66342dce64e2c979feb7f792b14368a502f474912034e00233425cfc3343f
+size 6603
--- server/src/main/java/com/najackdo/server/domain/book/service/UserBooksService.java
+++ server/src/main/java/com/najackdo/server/domain/book/service/UserBooksService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e7812b3f2526aa77ccd0f95195679b7be72f0d9b756957c356199fb36a6a1d34
-size 9119
+oid sha256:01c185ad11d8b34c43e866a1fdbcd3987d6d23fd39d0ad63615eaf2aedc81b8f
+size 9415
