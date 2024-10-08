388459aa4691a36412a3fd0cfb68014a92eb13ff
김도영 <rlaehdud1002@naver.com>
2024-10-08 17:51:47 +0900

    [FIX] 유효성 검사 로직 수정, 리뷰 수정
    Former-commit-id: c2017e2bbf044b8fc6a721c39de833c8db8d0e5d

--- client/src/page/chatting/components/ReviewButton.tsx
+++ client/src/page/chatting/components/ReviewButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05dcab09ba1ad29143de937d47ba0bb04581e1c8c83ae84b5abb33ef9f5d7b07
-size 589
+oid sha256:9ed5099acb6cfc887bd11867994b75881b50c72b8fb12bbafbcffa6131263233
+size 855
