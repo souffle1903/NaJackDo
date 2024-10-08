e92bfe2891910c722422c9c296a83db55ecec659
SeongMin Hwang <qq221qq@naver.com>
2024-10-08 09:54:23 +0900

    [FIX] API 스펙 추가 책 제목
    Former-commit-id: c45cb45654ef0387c4fdd1882f587d4c1e349062

--- client/src/page/main/components/CategoryRecommend.tsx
+++ client/src/page/main/components/CategoryRecommend.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d105c6ee933803306f0f303a1d6a7dc9794b821cbf9ee30959a4e3c39e19e82
-size 4022
+oid sha256:7365fa4b41cf7356f48aab401bad7816d3f74c2f574ff00fa3382bf5fd4f9893
+size 4034
