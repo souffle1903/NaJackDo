54a7e4ce99325ddd24510cb676e812379a7e27d5
서민수 <minsu9812@naver.com>
2024-10-08 12:11:07 +0900

    [FIX] ui 수정
    Former-commit-id: 66f48a785ba1f43d8dbe1b0f2603db5b7c062777

--- client/src/page/library/page/LibraryPage.tsx
+++ client/src/page/library/page/LibraryPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6c11f92e1c621135601304ab19d067d9d4450e920740a412a30c0fa57801559e
+oid sha256:6e5d3e298affbad6c90a3a6e0817c40cea0b6f28b4c9fca1f8d18f4fc37f1e71
 size 6001
