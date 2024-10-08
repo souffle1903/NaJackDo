9dcc6b3017650bdc41beae41ec6d4351a5e1c415
서민수 <minsu9812@naver.com>
2024-10-08 11:21:21 +0900

    [Fix] 디스플레이 수정
    Former-commit-id: bdb64b868ed833beaab8574da51818dd3a53a30c

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22700cf98fbb782d86aff46a967a9d67f253d370e4ae3770bc2e5d58d566fc3d
-size 5490
+oid sha256:ecdf293ce679be795f24e2110db229a359e111af835217c0a2201c6870859872
+size 5589
