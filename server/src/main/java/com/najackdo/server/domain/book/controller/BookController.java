4ce042dfb30c4da268def8581962c06da83e3e12
이인준 <dldlswns890@naver.com>
2024-10-08 16:57:45 +0900

    [HOTFIX] 병합 충돌 에러
    Former-commit-id: 1f014aa8202ccb4212d2ee644c87ab56a8ad5d13

--- server/src/main/java/com/najackdo/server/domain/book/controller/BookController.java
+++ server/src/main/java/com/najackdo/server/domain/book/controller/BookController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a9ddc9520328aebee39b04b5f805d867e04c3ddca5a3f027e2dc66974144dee
-size 6627
+oid sha256:4c69ee5d86aee88a2f91ef46348f9a17b136326e79a1b61aa4b80e8e6ac1fa1d
+size 6602
