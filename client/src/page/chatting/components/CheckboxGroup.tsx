c86d80e48a5afec5a24f7a1dc3259700c9c2026f
김도영 <rlaehdud1002@naver.com>
2024-10-08 11:18:23 +0900

    [FIX] 책 등록 텍스트 검색 무한 스크롤 적용
    Former-commit-id: a85699b921f0065b53408d8bdad421355ec31365

--- client/src/page/chatting/components/CheckboxGroup.tsx
+++ client/src/page/chatting/components/CheckboxGroup.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:862edea3d77c226b29d755df14b1f17f6bccfa63d139d9689b964466da87c8d2
-size 3034
+oid sha256:febff7c09f5aa826e4acdbdfde3408547c5ac7194090d33ab4681598d3f36879
+size 3049
