2194333b23183b8e8a5e13ce955d7066439ad293
김도영 <rlaehdud1002@naver.com>
2024-10-08 11:18:23 +0900

    [FIX] 책 등록 텍스트 검색 무한 스크롤 적용
    Former-commit-id: 332a08cc4e9d863a9760a053f56feda1be9ee4d8

--- client/src/atoms/Book.type.ts
+++ client/src/atoms/Book.type.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9f0051ce1b3b475c58484d7947a22970d5586025472546cba501d2f40a8a6eb5
-size 1302
+oid sha256:e1be8650cf9ce028824d590ffa185924743eefb1380986b6bb5e6dcb5ede2089
+size 1259
