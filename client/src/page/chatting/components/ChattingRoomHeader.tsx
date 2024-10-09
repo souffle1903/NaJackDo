5666d7ada29a2350e731ef7c75ccb37a99169255
Seo Minsu <minsu9812@naver.com>
2024-10-09 22:18:11 +0900

    [fix] ui 수정
    Former-commit-id: fa1bd4ee7f4f81e3792e9c86560104268179e258

--- client/src/page/chatting/components/ChattingRoomHeader.tsx
+++ client/src/page/chatting/components/ChattingRoomHeader.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4a94e07c2bb86c2e4c269c01db7c44e735e97e0953dfb48bd49f7fbf074b2d71
-size 808
+oid sha256:0396bb4714534c5dda79cbfed90ea16d23b4ec5bcfed7f137c7291a2957f7147
+size 803
