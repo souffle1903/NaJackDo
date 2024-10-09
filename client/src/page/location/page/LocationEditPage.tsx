647cc9aebd675b027cdc4cf73c9d078a32ad5519
김도영 <rlaehdud1002@naver.com>
2024-10-09 21:20:03 +0900

    [FIX] location redirect 수정
    Former-commit-id: 8e8dd9cb9a61f18207fe5ee69c9babc64fa47524

--- client/src/page/location/page/LocationEditPage.tsx
+++ client/src/page/location/page/LocationEditPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:09a0ae02a2e71190945525374cafd9e002985a369e0a5a49a54624f936676ee9
-size 722
+oid sha256:b3734da4c0a5b7b528e9988455b3d89263aaec04ca5bfeb0b32c1838a1b458ad
+size 716
