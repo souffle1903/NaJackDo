842bf5771100b0141d8e8873478d69370ccf59e7
김도영 <rlaehdud1002@naver.com>
2024-10-09 18:57:43 +0900

    [FIX] padding 수정
    Former-commit-id: 7eef753a24549be4a5d0edb611685d8430f1faac

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1dffc68e864196f936b2238c349a3b5631a484bc8cb0f9c156c66ecac92d32e2
-size 5257
+oid sha256:8d9f1c46d60fde35870f52050f2395a83ec43cad4a987e1a7a14eef7f42dd9c6
+size 5528
