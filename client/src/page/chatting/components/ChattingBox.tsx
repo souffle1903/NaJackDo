1090d6e3cde962167a6bc747020ba9e1c0a83430
김도영 <rlaehdud1002@naver.com>
2024-10-09 21:59:32 +0900

    [FIX]
    Former-commit-id: 1f01e71b15fb04f751f99bf73cb4a7b5a988ff4f

--- client/src/page/chatting/components/ChattingBox.tsx
+++ client/src/page/chatting/components/ChattingBox.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0e8b5e69dfdda03c4a8bbd1563d173781472c789f490c46199618cb29134037
-size 4535
+oid sha256:ab84dff2ffb70f1c6cb9c84080ed2c830936ee126e26f95739fd9f20bbbb5917
+size 4560
