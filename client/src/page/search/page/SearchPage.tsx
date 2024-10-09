7fa5d363e4c2449802d2ec9bffc0fb5a64c487b9
김도영 <rlaehdud1002@naver.com>
2024-10-09 18:38:40 +0900

    [FIX] search recommend
    Former-commit-id: fb3438a763f42c46a936ff19a169b1167f9e40f4

--- client/src/page/search/page/SearchPage.tsx
+++ client/src/page/search/page/SearchPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c71951450df8ffa8200670df7c887e839104478b4e49abe842685e9f6cab86e8
-size 2714
+oid sha256:469190f06d2b1ba899ef09d1587a4530dc7159cf5386f942ace28b6cd26c4a0b
+size 2719
