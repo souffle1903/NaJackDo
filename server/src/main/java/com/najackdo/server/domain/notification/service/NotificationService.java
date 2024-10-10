6a392b4a11839bb041fa02fbb1857f4a3a03d9e1
이인준 <dldlswns890@naver.com>
2024-10-10 10:56:42 +0900

    [HOTFIX] 알림 에러 로그
    Former-commit-id: cead31f0b2baee364ec6987c7016cd532d274139

--- server/src/main/java/com/najackdo/server/domain/notification/service/NotificationService.java
+++ server/src/main/java/com/najackdo/server/domain/notification/service/NotificationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7b81c5877932aaec8852faf2ede67aa323e4cc1d714fad405ca610c50ff27c40
-size 4674
+oid sha256:d56df9fc8228cb42ca916db9202960873c8df852dad519b40c9119bcfe8bbf9a
+size 4678
