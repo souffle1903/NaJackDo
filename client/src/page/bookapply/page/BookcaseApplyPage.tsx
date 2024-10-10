7b748fce98688d577f38b3c380d1aa4494c792c3
김도영 <rlaehdud1002@naver.com>
2024-10-10 22:54:32 +0900

    [FIX] 책장 인식 결과 페이지 디자인 수정
    Former-commit-id: d754dafc574b688d2f299e98b300433518f9aab6

--- client/src/page/bookapply/page/BookcaseApplyPage.tsx
+++ client/src/page/bookapply/page/BookcaseApplyPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34424245c2e5603aa01581e32785db90648421be5a9feaffa374d5f67402c634
-size 3932
+oid sha256:36a4253aeb21a088fbad457ecd3d062ea756d2b7cff5a7cfd960eff268727a04
+size 4043
