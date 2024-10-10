9c2332d1d48cda6db18ca9b6d78352b261c5d44f
김도영 <rlaehdud1002@naver.com>
2024-10-10 13:57:15 +0900

    [FIX] notification
    Former-commit-id: caa0bc615d936591354b8fec01b796dc7a150b48

--- client/src/page/login/page/AuthCallbackPage.tsx
+++ client/src/page/login/page/AuthCallbackPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:10a39607eae65013913325f18f15f8301516f677909b14ab9d467699f35248f0
-size 1584
+oid sha256:db0ae27a3a40bbc130d5ce16a0c0fa010e0212aded07bf09f5ff9be089415c50
+size 1587
