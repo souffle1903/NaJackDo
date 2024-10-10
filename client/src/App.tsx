36750f0f8e72eb26e7539c02e83c4566321bbcc1
김도영 <rlaehdud1002@naver.com>
2024-10-10 22:59:55 +0900

    [FIX]
    [DOCS] ReadMe
    
    
    Former-commit-id: d1cec5a897a9320e0167cdb0c1ba716b3e5c0b9e

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8cdcfc3c518244e1051f00101ead81517c9e28050b621ddda51b465ce76d7141
-size 5754
+oid sha256:60822dd228a7c882478d84bbeec22956394b881fc9f9b15c7e4ece0e8a2a7f8b
+size 5481
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-C105 
+version https://git-lfs.github.com/spec/v1
+oid sha256:6cdc2bdff7c877683c4b729775290a8c2b517cedecb8f28322b168c194179ef8
+size 12766
