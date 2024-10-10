dab96216f9415301c1db8ad098942091629fa72a
SeongMin Hwang <qq221qq@naver.com>
2024-10-10 16:39:10 +0900

    ^-^
    Former-commit-id: 97ca3d5d285e548f078600cb70db861c6a612a61

--- server/src/main/resources/recommendation/app/clovar.py
+++ server/src/main/resources/recommendation/app/clovar.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ec9f18b9a794fe4d812997b71dab221bc91d699ff685cfcd1c605d4d3c97297
-size 1272
+oid sha256:3aa9a61ecafba66df5e49c46221970573f7a5c0fece3c3d0535b1d1998c4a3b8
+size 1323
