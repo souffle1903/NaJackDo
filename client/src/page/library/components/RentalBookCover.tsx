a48ddae9b4d8c9694250c330ded5fb4b337e256d
서민수 <minsu9812@naver.com>
2024-10-10 11:33:16 +0900

    UI 수정
    Former-commit-id: 7f96bcc23e6cd033ba003b1850d5b07a3ac794f6

--- client/src/page/library/components/RentalBookCover.tsx
+++ client/src/page/library/components/RentalBookCover.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3229b5edbc0783f67e6953cf7cdf98e2dd337996495c7a4ee7ea937cace47db8
-size 782
+oid sha256:d7174c85ea00ea6bd9fdbf4381ad033c158e12754050982b2b3fb0d35cb330b1
+size 809
