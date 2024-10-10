7e9fbcd540bc08654af976745ed50a66a2210385
SeongMin Hwang <qq221qq@naver.com>
2024-10-10 12:10:30 +0900

    알람 다 표시
    Former-commit-id: 7e0b3753b9682d1e55f343d56c08a160af50af13

--- server/src/main/java/com/najackdo/server/core/configuration/WebSocketConfig.java
+++ server/src/main/java/com/najackdo/server/core/configuration/WebSocketConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3185f5f2bb8b838a3b6ab452ac10f1f5994170cf20c6e9e4bc221189a6aaf9d7
-size 1904
+oid sha256:6889ef92b6a38edf39a348a92bcf35ffb3a950628852af791302d5cf61ad5e30
+size 1903
--- server/src/main/java/com/najackdo/server/domain/notification/service/NotificationService.java
+++ server/src/main/java/com/najackdo/server/domain/notification/service/NotificationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:46c9e4d4ab35c9447f0496b84990e6fb9eeb938527491db9a3a4d72d0478d98e
-size 4166
+oid sha256:e4c87c166bca85636941fe671f74e7bbfc084827f0145cfe8978bb75aa57fbda
+size 4307
