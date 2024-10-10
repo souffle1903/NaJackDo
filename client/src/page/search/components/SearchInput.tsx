3b874152d5dcf95c8c6f14e9054f97806d76bb99
김도영 <rlaehdud1002@naver.com>
2024-10-10 21:55:20 +0900

    [FIX] AlertModal
    Former-commit-id: 5e75b7d04ce49925e15e8b229a7961a5242ac593

--- client/src/page/search/components/SearchInput.tsx
+++ client/src/page/search/components/SearchInput.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e87fdfb0f0668b8e7b95fa855c5500222d332af4afcd7ffc893a1f365b353ca1
-size 1384
+oid sha256:c0d038ed5ac0b07cd1355211ac1e1e323a5ceb371f8ef0fe604b657325012ff7
+size 1657
