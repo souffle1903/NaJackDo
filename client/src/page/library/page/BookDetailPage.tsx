060547ae7b8d38dba8d1d6e2536068452aca1224
김도영 <rlaehdud1002@naver.com>
2024-10-10 22:39:12 +0900

    [FIX]
    Former-commit-id: b1fec05e5386156da6be0a61dffd20bfc67e0102

--- client/src/page/library/page/BookDetailPage.tsx
+++ client/src/page/library/page/BookDetailPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a2dad2d8c208bf6b12098561a34f8f33fc0e75fcd4bcb5f4ce9603aa0717c6fa
-size 1472
+oid sha256:32733574c8d5d52ded726c104fe3d5d609a95469ba911d9323feb657af4ea94d
+size 1764
