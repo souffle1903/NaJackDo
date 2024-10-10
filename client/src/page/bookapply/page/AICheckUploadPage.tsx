45c2de536767dad8d049683943a505fae7d5bdca
lij0825 <dldlswns890@gmail.com>
2024-10-10 21:40:28 +0900

    [FEAT] 연결 및 디자인
    Former-commit-id: 95fb4573766b129dc213a33c204f41837d6426aa

--- client/src/page/bookapply/page/AICheckUploadPage.tsx
+++ client/src/page/bookapply/page/AICheckUploadPage.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:41ba1c7c42ac008f89282e35680d8c4273fab069cd48db9cafac726d720cc2ae
-size 6002
+oid sha256:49d9cdc720ee503155b45e361a5e77d1fbc8d65f0e6d4184e0ee9840516c7141
+size 6613
